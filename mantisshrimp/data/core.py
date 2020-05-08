becf88e833e16d1289c1eb754538a20328513d7c
lgvaz <lucasgouvaz@gmail.com>
2020-05-08 11:46:57 -0300

    implements mask_file

--- mantisshrimp/data/core.py
+++ mantisshrimp/data/core.py
@@ -7,15 +7,18 @@ from ..imports import *
 from ..core import *
 
 # Cell
-def open_img(fn):
+def open_img(fn, gray=False):
     if not os.path.exists(fn): raise ValueError(f'File {fn} does not exists')
-    return cv2.cvtColor(cv2.imread(str(fn)), cv2.COLOR_BGR2RGB)
+    color = cv2.COLOR_BGR2GRAY if gray else cv2.COLOR_BGR2RGB
+    return cv2.cvtColor(cv2.imread(str(fn)), color)
 
 # Cell
 def show_img(im, ax=None, **kwargs):
     if ax is None: fig,ax = plt.subplots(**kwargs)
+    im = im.squeeze()
+    cmap = 'gray' if len(im.shape)==2 else None
+    ax.imshow(im, cmap=cmap)
     ax.set_axis_off()
-    ax.imshow(im.squeeze())
     return ax
 
 # Cell
