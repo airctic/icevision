80ea88228c5fe7784239b2b99fa6c1b9d519be2c
lgvaz <lucasgouvaz@gmail.com>
2020-05-17 19:03:52 -0300

    closes #20

--- mantisshrimp/imports.py
+++ mantisshrimp/imports.py
@@ -12,13 +12,18 @@ from typing import *
 
 from torch import tensor, Tensor
 from torch.utils.data import DataLoader
+from torch.optim import SGD,Adam,AdamW,Adagrad,Adadelta,RMSprop
+from torch.optim.lr_scheduler import (LambdaLR,StepLR,MultiStepLR,MultiplicativeLR,OneCycleLR,
+                                      CosineAnnealingLR, CosineAnnealingWarmRestarts)
+
 from torchvision.transforms.functional import to_tensor as im2tensor
 from torchvision.models.detection.generalized_rcnn import GeneralizedRCNN
 from torchvision.models.detection.faster_rcnn import FasterRCNN,FastRCNNPredictor,fasterrcnn_resnet50_fpn
 from torchvision.models.detection.mask_rcnn import MaskRCNN,MaskRCNNPredictor,maskrcnn_resnet50_fpn
 from torchvision.models.detection.keypoint_rcnn import KeypointRCNN,KeypointRCNNPredictor,keypointrcnn_resnet50_fpn
-from pytorch_lightning import LightningModule, Trainer
 
+from pytorch_lightning import LightningModule, Trainer
+from pytorch_lightning.trainer.distrib_parts import get_all_available_gpus
 from pytorch_lightning.callbacks import LearningRateLogger
 
 from fastcore.imports import *
