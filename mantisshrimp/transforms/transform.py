606e13e8a884e5729cddf8505960d067e3711092
lgvaz <lucasgouvaz@gmail.com>
2020-05-31 14:44:30 -0300

    adds abstract method apply to Transform

--- mantisshrimp/transforms/transform.py
+++ mantisshrimp/transforms/transform.py
@@ -1,10 +1,29 @@
 __all__ = ["Transform"]
 
+from ..imports import *
+from ..core import *
 
-class Transform:
+
+class Transform(ABC):
     def __init__(self, tfms):
         self.tfms = tfms
 
-    def __call__(self, item):
+    def __call__(self, item: Item):
         tfmed = self.apply(**item.asdict())
         return item.replace(**tfmed)
+
+    @abstractmethod
+    def apply(
+        self,
+        img: np.ndarray,
+        imageid: int,
+        labels: List[int],
+        iscrowds: List[int],
+        bboxes: List[BBox],
+        masks: MaskArray,
+    ):
+        """ Apply the transform
+        Returns:
+              dict: Modified values, the keys of the dictionary should have the same
+              names as the keys received by this function
+        """
