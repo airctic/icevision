7ae0fc7eba5d057272be9a8128625cf2abaeebc2
lgvaz <lucasgouvaz@gmail.com>
2020-06-02 18:39:26 -0300

    updates transform to new data structure

--- mantisshrimp/transforms/albu_transform.py
+++ mantisshrimp/transforms/albu_transform.py
@@ -10,35 +10,35 @@ class AlbuTransform(Transform):
     def __init__(self, tfms):
         import albumentations as A
 
-        self.bbox_params = A.BboxParams(format="pascal_voc", label_fields=["labels"])
+        self.bbox_params = A.BboxParams(format="pascal_voc", label_fields=["label"])
         super().__init__(tfms=A.Compose(tfms, bbox_params=self.bbox_params))
 
     def apply(
         self,
         img: np.ndarray,
-        labels=None,
-        bboxes: BBox = None,
-        masks: MaskArray = None,
-        iscrowds: List[int] = None,
+        label=None,
+        bbox: List[BBox] = None,
+        mask: MaskArray = None,
+        iscrowd: List[int] = None,
         **kwargs
     ):
-        # Substitue labels with list of idxs, so we can also filter out iscrowd in case any bbox is removed
+        # Substitue label with list of idxs, so we can also filter out iscrowd in case any bbox is removed
         # TODO: Same should be done if a mask is completely removed from the image (if bbox is not given)
         params = {"image": img}
-        params["labels"] = list(range_of(labels)) if labels is not None else []
-        params["bboxes"] = [bbox.xyxy for bbox in bboxes] if bboxes is not None else []
-        if masks is not None:
-            params["masks"] = masks.data
+        params["label"] = list(range_of(label)) if label is not None else []
+        params["bboxes"] = [o.xyxy for o in bbox] if bbox is not None else []
+        if mask is not None:
+            params["masks"] = mask.data
 
         d = self.tfms(**params)
 
         out = {"img": d["image"]}
-        if labels is not None:
-            out["labels"] = [labels[i] for i in d["labels"]]
-        if bboxes is not None:
-            out["bboxes"] = [BBox.from_xyxy(*points) for points in d["bboxes"]]
-        if masks is not None:
-            out["masks"] = MaskArray(np.stack(d["masks"]))
-        if iscrowds is not None:
-            out["iscrowds"] = [iscrowds[i] for i in d["labels"]]
+        if label is not None:
+            out["label"] = [label[i] for i in d["label"]]
+        if bbox is not None:
+            out["bbox"] = [BBox.from_xyxy(*points) for points in d["bboxes"]]
+        if mask is not None:
+            out["mask"] = MaskArray(np.stack(d["masks"]))
+        if iscrowd is not None:
+            out["iscrowd"] = [iscrowd[i] for i in d["label"]]
         return out
