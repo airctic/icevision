b3330e264f53f3f6f3490b1c4beef8f1c931b235
lgvaz <lucasgouvaz@gmail.com>
2020-06-03 20:02:55 -0300

    makes RandomSplitter a class

--- mantisshrimp/parsers/splits.py
+++ mantisshrimp/parsers/splits.py
@@ -1,4 +1,4 @@
-__all__ = ["DataSplit", "random_split", "RandomSplitter"]
+__all__ = ["DataSplit", "random_split", "DataSplitter", "RandomSplitter"]
 
 from mantisshrimp.imports import *
 from mantisshrimp.utils import *
@@ -16,16 +16,27 @@ def random_split(p=None, splits=None):
     return np.random.choice(splits, p=p)
 
 
-def RandomSplitter(probs, seed=None):
-    def _inner(ids):
+class DataSplitter(ABC):
+    def __call__(self, ids):
+        return self.split(ids)
+
+    @abstractmethod
+    def split(self, ids):
+        pass
+
+
+class RandomSplitter(DataSplitter):
+    def __init__(self, probs, seed=None):
+        self.probs = probs
+        self.seed = seed
+
+    def split(self, ids):
         # calculate split indexes
-        p = np.array(probs) * len(ids)  # convert percentage to absolute
+        p = np.array(self.probs) * len(ids)  # convert percentage to absolute
         p = np.ceil(p).astype(int)  # round up, so each split has at least one example
         p[p.argmax()] -= sum(p) - len(ids)  # removes excess from split with most items
         p = np.cumsum(p)
 
-        with np_local_seed(seed):
+        with np_local_seed(self.seed):
             shuffled = np.random.permutation(list(ids))
         return np.split(shuffled, p.tolist())[:-1]  # last element is always empty
-
-    return _inner
