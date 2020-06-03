ec29e347218332ad3a29ea1dd073dc39b0fa21a6
lgvaz <lucasgouvaz@gmail.com>
2020-06-03 02:22:27 -0300

    adds default parsers

---
+++ mantisshrimp/parsers/defaults.py
@@ -0,0 +1,21 @@
+__all__ = ["ImageInfoParser", "FasterRCNNParser", "MaskRCNNParser"]
+
+from mantisshrimp.imports import *
+from mantisshrimp.parsers.parser import *
+from mantisshrimp.parsers.mixins import *
+
+
+class ImageInfoParser(
+    Parser, FilepathParserMixin, SizeParserMixin, ABC,
+):
+    pass
+
+
+class FasterRCNNParser(
+    Parser, LabelParserMixin, BBoxParserMixin, IsCrowdParserMixin, ABC
+):
+    pass
+
+
+class MaskRCNNParser(FasterRCNNParser, MaskParserMixin, ABC):
+    pass
