cea79093ad74330308ba29c8ac35c2ef20621d3a
lgvaz <lucasgouvaz@gmail.com>
2020-06-06 20:48:44 -0300

    implements prepare_record functional style

--- mantisshrimp/visualize/show_record.py
+++ mantisshrimp/visualize/show_record.py
@@ -1,7 +1,7 @@
 __all__ = ["show_record"]
 
 from mantisshrimp.imports import *
-from mantisshrimp.data_preparer import *
+from mantisshrimp.data import *
 from .show_annotation import *
 
 
@@ -12,9 +12,9 @@ def show_record(
     mask: bool = True,
     ax: plt.Axes = None,
     show: bool = False,
-    data_preparer: DataPreparer = None,
+    prepare_record=None,
 ):
-    data_preparer = data_preparer or DefaultDataPreparer()
+    data_preparer = prepare_record or default_prepare_record
     data = data_preparer(record)
     return show_annotation(
         data["img"],
