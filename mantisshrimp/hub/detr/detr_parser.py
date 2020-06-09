271f9e9f790a9c3601f30b1f1baa2397d2fb7fb8
lgvaz <lucasgouvaz@gmail.com>
2020-06-09 18:26:56 -0300

    adds detr repo as submodule, adds detr_wheat example

---
+++ mantisshrimp/hub/detr/detr_parser.py
@@ -0,0 +1,16 @@
+__all__ = ["DetrBBoxParser", "DetrMaskParser"]
+
+from mantisshrimp.imports import *
+from mantisshrimp import *
+
+
+class DetrBBoxParser(DefaultImageInfoParser, FasterRCNNParser, IsCrowdParserMixin, ABC):
+    """
+    This parser contain all the required fields for using Detr for bbox detection.
+    """
+
+
+class DetrMaskParser(DetrBBoxParser, MaskRCNNParser, ABC):
+    """
+    This parser contain all the required fields for using Detr for bbox detection.
+    """
