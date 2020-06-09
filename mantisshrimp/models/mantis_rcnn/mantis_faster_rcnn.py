2d9d49bde379d9bb797f3bfb7fd1458a73bf248f
Aditya Oke <47158509+oke-aditya@users.noreply.github.com>
2020-06-10 00:17:47 +0530

    reformatted with black

--- mantisshrimp/models/mantis_rcnn/mantis_faster_rcnn.py
+++ mantisshrimp/models/mantis_rcnn/mantis_faster_rcnn.py
@@ -6,26 +6,51 @@ from mantisshrimp.models.mantis_rcnn.rcnn_param_groups import *
 from mantisshrimp.models.mantis_rcnn.mantis_rcnn import *
 from mantisshrimp.backbones import *
 
+
 class MantisFasterRCNN(MantisRCNN):
     @delegates(FasterRCNN.__init__)
-    def __init__(self, n_class, h=256, backbone="default", pretrained=True, metrics=None, **kwargs):
+    def __init__(
+        self,
+        n_class,
+        h=256,
+        backbone="default",
+        pretrained=True,
+        metrics=None,
+        **kwargs,
+    ):
         super().__init__(metrics=metrics)
-        self.n_class, self.h, self.backbone, self.pretrained = n_class, h, backbone, pretrained
+        self.n_class, self.h, self.backbone, self.pretrained = (
+            n_class,
+            h,
+            backbone,
+            pretrained,
+        )
 
-        if(self.backbone == "defualt"):
+        if self.backbone == "defualt":
             # Creates the default fasterrcnn as given in pytorch. Trained on COCO dataset
             self.m = fasterrcnn_resnet50_fpn(pretrained=self.pretrained, **kwargs)
             in_features = self.m.roi_heads.box_predictor.cls_score.in_features
-            self.m.roi_heads.box_predictor = FastRCNNPredictor(in_features, self.n_class)
+            self.m.roi_heads.box_predictor = FastRCNNPredictor(
+                in_features, self.n_class
+            )
         else:
             # Creates the custom backbone model trained on ImageNet.
             try:
-                self.base_model = create_torchvision_backbone(backbone=self.backbone, is_pretrained=pretrained)
-                self.ft_anchor_generator = AnchorGenerator(sizes=((32, 64, 128)), aspect_ratios=((0.5, 1.0, 2.0)))
-                self.ft_mean = [0.485, 0.456, 0.406] # imagenet mean and std
+                self.base_model = create_torchvision_backbone(
+                    backbone=self.backbone, is_pretrained=pretrained
+                )
+                self.ft_anchor_generator = AnchorGenerator(
+                    sizes=((32, 64, 128)), aspect_ratios=((0.5, 1.0, 2.0))
+                )
+                self.ft_mean = [0.485, 0.456, 0.406]  # imagenet mean and std
                 self.ft_std = [0.229, 0.224, 0.225]
-                self.m = FasterRCNN(backbone=self.base_model, num_classes=self.n_class, image_mean=self.ft_mean, image_std=self.ft_std)
-            except NotImplementedError :
+                self.m = FasterRCNN(
+                    backbone=self.base_model,
+                    num_classes=self.n_class,
+                    image_mean=self.ft_mean,
+                    image_std=self.ft_std,
+                )
+            except NotImplementedError:
                 print("Invalid Backbone")
 
     def forward(self, images, targets=None):
