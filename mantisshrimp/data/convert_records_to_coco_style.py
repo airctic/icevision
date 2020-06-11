dcbd757e0cce60fb6169cda553bb5a5e07f6eee4
lgvaz <lucasgouvaz@gmail.com>
2020-06-11 17:15:55 -0300

    implements convert_records_to_coco_style

---
+++ mantisshrimp/data/convert_records_to_coco_style.py
@@ -0,0 +1,76 @@
+__all__ = ["convert_records_to_coco_style", "coco_api_from_records"]
+
+from pycocotools.coco import COCO
+from mantisshrimp.imports import *
+from mantisshrimp import *
+
+
+def coco_api_from_records(records):
+    coco_ds = COCO()
+    coco_ds.dataset = convert_records_to_coco_style(records)
+    coco_ds.createIndex()
+    return coco_ds
+
+
+def convert_records_to_coco_style(records):
+    """ Converts records from library format to coco format.
+    Inspired from: https://github.com/pytorch/vision/blob/master/references/detection/coco_utils.py#L146
+    """
+    images = []
+    annotations_dict = defaultdict(list)
+    categories_set = set()
+
+    for record in pbar(records):
+        # build images field
+        image = {}
+        image["id"] = record["imageid"]
+        image["file_name"] = Path(record["filepath"]).name
+        image["width"] = record["width"]
+        image["height"] = record["height"]
+        images.append(image)
+
+        # build annotations field
+        for label in record["label"]:
+            annotations_dict["image_id"].append(record["imageid"])
+            annotations_dict["category_id"].append(label)
+            categories_set.add(label)
+
+        if "bbox" in record:
+            for bbox in record["bbox"]:
+                annotations_dict["bbox"].append(bbox.xywh)
+
+        if "area" in record:
+            for area in record["area"]:
+                annotations_dict["area"].append(area)
+
+        if "mask" in record:
+            for mask in record["mask"]:
+                if isinstance(mask, Polygon):
+                    annotations_dict["segmentation"].append(mask.points)
+                elif isinstance(mask, RLE):
+                    coco_rle = {
+                        "counts": mask.to_coco(),
+                        "size": [record["height"], record["width"]],
+                    }
+                    annotations_dict["segmentation"].append(coco_rle)
+                else:
+                    msg = f"Mask type {type(mask)} unsupported, we only support RLE and Polygon"
+                    raise ValueError(msg)
+
+        for iscrowd in record["iscrowd"]:
+            annotations_dict["iscrowd"].append(iscrowd)
+
+    if not allequal([len(o) for o in annotations_dict.values()]):
+        raise RuntimeError("Mismatch lenght of elements")
+
+    categories = [{"id": i} for i in categories_set]
+
+    # convert dict of lists to list of dicts
+    annotations = []
+    for i in range(len(annotations_dict["image_id"])):
+        annotation = {k: v[i] for k, v in annotations_dict.items()}
+        # annotations should be initialized starting at 1 (torchvision issue #1530)
+        annotation["id"] = i + 1
+        annotations.append(annotation)
+
+    return {"images": images, "annotations": annotations, "categories": categories}
