75b96967662602df4ed5b1df85322e8215935854
Aditya Oke <47158509+oke-aditya@users.noreply.github.com>
2020-06-12 21:24:12 +0530

    Ooops I had deleted some code :-( readded it

--- mantisshrimp/models/mantis_rcnn/mantis_faster_rcnn.py
+++ mantisshrimp/models/mantis_rcnn/mantis_faster_rcnn.py
@@ -86,3 +86,24 @@ class MantisFasterRCNN(MantisRCNN):
             else:
                 raise ValueError(f"{name} not supported")
         return backbone
+    
+    def forward(self, images, targets=None):
+        return self.m(images, targets)
+
+    def model_splits(self):
+        return split_rcnn_model(self.m)
+
+    @staticmethod
+    def build_training_sample(
+        imageid: int, img: np.ndarray, label: List[int], bbox: List[BBox], **kwargs,
+    ):
+        x = im2tensor(img)
+        # inject values when annotations are empty are disconsidered
+        # because we mark label as 0 (background)
+        _fake_box = [0, 1, 2, 3]
+        y = {
+            "image_id": tensor(imageid, dtype=torch.int64),
+            "labels": tensor(label or [0], dtype=torch.int64),
+            "boxes": tensor([o.xyxy for o in bbox] or [_fake_box], dtype=torch.float),
+        }
+        return x, y
