8b5676578934f2f625203519ed887bf1ffb57cb3
Aditya Oke <47158509+oke-aditya@users.noreply.github.com>
2020-06-17 22:55:18 +0530

    fixed hidden size issue

--- mantisshrimp/models/mantis_rcnn/mantis_mask_rcnn.py
+++ mantisshrimp/models/mantis_rcnn/mantis_mask_rcnn.py
@@ -16,20 +16,6 @@ class MantisMaskRCNN(MantisRCNN):
         super().__init__(metrics=metrics)
         self.num_classes = num_classes
         self.backbone = backbone
-        # model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
-
-        # # get the number of input features for the classifier
-        # in_features = model.roi_heads.box_predictor.cls_score.in_features
-        # # replace the pre-trained head with a new one
-        # model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-
-        # # now get the number of input features for the mask classifier
-        # in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
-        # hidden_layer = 256
-        # # and replace the mask predictor with a new one
-        # model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
-        #                                                    hidden_layer,
-        #                                                    num_classes)
 
         if backbone is None:
             # Creates the default fasterrcnn as given in pytorch. Trained on COCO dataset
@@ -40,8 +26,9 @@ class MantisMaskRCNN(MantisRCNN):
 
             self.m.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
             in_features_mask = self.m.roi_heads.mask_predictor.conv5_mask.in_channels
+            hidden_layer = 256
             self.m.roi_heads.mask_predictor = MaskRCNNPredictor(
-                in_features_mask, num_classes
+                in_features_mask, hidden_layer, num_classes
             )
 
         else:
