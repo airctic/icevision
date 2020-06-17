59bc70a207b45df80154e188c5e9bb1356331edb
Aditya Oke <47158509+oke-aditya@users.noreply.github.com>
2020-06-17 22:40:42 +0530

    fixed dataloader

--- tests/models/test_rcnn_dataloader.py
+++ tests/models/test_rcnn_dataloader.py
@@ -4,7 +4,7 @@ from mantisshrimp import *
 
 def test_mask_rcnn_dataloader(records):
     dataset = Dataset(records)
-    dl = MantisMaskRCNN(num_classes=2, backbone=None).dataloader(dataset, batch_size=3)
+    dl = MantisMaskRCNN.dataloader(dataset, batch_size=3)
     xb, yb = first(dl)
     assert len(xb) == 3
     assert len(yb) == 3
