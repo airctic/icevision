7d73ec3e9c33026b122fa2eaacac70b3e7d12ce6
lgvaz <lucasgouvaz@gmail.com>
2020-06-21 19:01:17 -0300

    adds comments

--- examples/maskrcnn_fastai.py
+++ examples/maskrcnn_fastai.py
@@ -3,7 +3,7 @@ from mantisshrimp import *
 from mantisshrimp.hub.pennfundan import *
 from mantisshrimp.engines.fastai import *
 
-# Unified setup
+### Unified setup ###
 source = get_pennfundan_data()
 parser = PennFundanParser(source)
 splitter = RandomSplitter([0.8, 0.2])
@@ -13,14 +13,16 @@ valid_dataset = Dataset(valid_records)
 model = MantisMaskRCNN(num_classes=2)
 train_dataloader = model.dataloader(train_dataset, batch_size=2, num_workers=2)
 valid_dataloader = model.dataloader(valid_dataset, batch_size=2, num_workers=2)
+metric = COCOMetric(valid_records, bbox=True, mask=True)
 ###
 
-metric = COCOMetric(valid_records, bbox=True, mask=True)
+### Engine dependent ###
 learn = rcnn_learner(
     dls=[train_dataloader, valid_dataloader], model=model, metrics=[metric]
 )
 
 learn.fine_tune(3, lr=2e-4)
+###
 
 # TODO: add some tests
 # check that model_splits is freezing the correct layers
