67f3e1a8fc4f650908870c1ac5785e4b682deac3
lgvaz <lucasgouvaz@gmail.com>
2020-06-26 16:09:51 -0300

    adds voc hub

---
+++ mantisshrimp/hub/voc/voc_data.py
@@ -0,0 +1,24 @@
+__all__ = ["get_voc_data"]
+
+from mantisshrimp.imports import *
+from mantisshrimp import *
+
+
+def get_voc_data(force_download=False):
+    url = "http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar"
+
+    save_dir = get_data_dir() / "voc"
+    save_dir.mkdir(exist_ok=True)
+    tar_file = save_dir / "trainval.tar"
+
+    if not tar_file.exists() or force_download:
+        download_url(url=url, save_path=tar_file)
+        # extract file
+        shutil.unpack_archive(tar_file, save_dir)
+        # move extract files so they are placed at save_dir
+        files_dir = save_dir / "VOCdevkit/VOC2012"
+        for file in files_dir.ls():
+            shutil.move(str(file), save_dir)
+        shutil.rmtree(files_dir.parent)
+
+    return save_dir
