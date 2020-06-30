7010488e5e9dd492689bb1741ff335902387854e
Aditya Oke <47158509+oke-aditya@users.noreply.github.com>
2020-06-30 18:59:49 +0530

    Fixed up final error

--- mantisshrimp/backbones/__init__.py
+++ mantisshrimp/backbones/__init__.py
@@ -1,5 +1,3 @@
 from mantisshrimp.backbones.torchvision_backbones import *
 from torchvision.models.detection.backbone_utils import *
 from mantisshrimp.backbones.create_backbones import *
-from mantisshrimp.backbones.timm_backbones import *
-from mantisshrimp.backbones.torchvision_backbones import *
