a2bf6ae8f8fd9eb96e9c5d694d157f61692c883b
lgvaz <lucasgouvaz@gmail.com>
2020-07-01 14:32:12 -0300

    imports resnet_fpn_backbone from torchvision

--- mantisshrimp/backbones/__init__.py
+++ mantisshrimp/backbones/__init__.py
@@ -1,2 +1,5 @@
 from mantisshrimp.backbones.torchvision_backbones import *
-from torchvision.models.detection.backbone_utils import BackboneWithFPN
+from torchvision.models.detection.backbone_utils import (
+    BackboneWithFPN,
+    resnet_fpn_backbone,
+)
