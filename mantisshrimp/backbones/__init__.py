d08d1f5da7df08a42aaba76ac1cef69ef4a5d031
Aditya Oke <47158509+oke-aditya@users.noreply.github.com>
2020-07-03 00:29:55 +0530

    Made changes, removed comments, fixed __init__, requirements

--- mantisshrimp/backbones/__init__.py
+++ mantisshrimp/backbones/__init__.py
@@ -1,3 +1,6 @@
 from mantisshrimp.backbones.torchvision_backbones import *
-from torchvision.models.detection.backbone_utils import *
+from torchvision.models.detection.backbone_utils import (
+    BackboneWithFPN,
+    resnet_fpn_backbone,
+)
 from mantisshrimp.backbones.create_backbones import *
--- mantisshrimp/backbones/create_backbones.py
+++ mantisshrimp/backbones/create_backbones.py
@@ -13,16 +13,18 @@ def create_backbone_generic(model, out_channels: int):
     ft_backbone.out_channels = out_channels
     return ft_backbone
 
+
 # Use this when you have Adaptive Pooling layer in End.
 # When Model.features is not applicable.
 def create_backbone_adaptive(model, out_channels: int = None):
     # Out channels is optional can pass it if user knows it
-    # print(list(model.children())[-1].in_features)    
+    # print(list(model.children())[-1].in_features)
     if out_channels is None:
         # Tries to infer the out_channels from layer before adaptive avg pool.
         modules_total = list(model.children())
         out_channels = modules_total[-1].in_features
     return create_backbone_generic(model, out_channels=out_channels)
+
 
 # Use this when model.features function is available as in mobilenet and vgg
 # Again it is mandatory for out_channels here
@@ -30,4 +32,3 @@ def create_backbone_features(model, out_channels: int):
     ft_backbone = model.features
     ft_backbone.out_channels = out_channels
     return ft_backbone
-
