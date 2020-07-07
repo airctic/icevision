89a58743b4ae106dbef8aff4b2a77ea7563a61c0
lgvaz <lucasgouvaz@gmail.com>
2020-07-07 16:06:54 -0300

    cleans tests

--- tests/models/test_efficientdet.py
+++ tests/models/test_efficientdet.py
@@ -1,53 +1,68 @@
+import pytest
 from mantisshrimp.imports import *
 from mantisshrimp import *
 
-img = 255 * np.ones((4, 4, 3), dtype=np.uint8)
-bboxes = [BBox.from_xyxy(1, 2, 3, 4), BBox.from_xyxy(4, 3, 2, 1)]
-labels = [1, 2]
+
+@pytest.fixture()
+def img():
+    return 255 * np.ones((4, 4, 3), dtype=np.uint8)
 
 
-## Build a single sample
-def build_train_sample(img: np.ndarray, labels: List[int], bboxes: List[BBox]):
-    x = im2tensor(img)
-
-    y = {
-        "cls": tensor(labels, dtype=torch.int64),
-        "boxes": tensor([bbox.yxyx for bbox in bboxes], dtype=torch.float64),
-    }
-
-    return x, y
+@pytest.fixture()
+def labels():
+    return [1, 2]
 
 
-image, target = build_train_sample(img=img, labels=labels, bboxes=bboxes)
-
-assert torch.all(image == torch.ones((3, 4, 4), dtype=torch.float32))
-
-assert torch.all(target["cls"] == tensor([1, 2], dtype=torch.int64))
-assert torch.all(
-    target["boxes"] == tensor([[2, 1, 4, 3], [3, 4, 1, 2]], dtype=torch.float64)
-)
-
-## Dataloader
+@pytest.fixture()
+def bboxes():
+    return [BBox.from_xyxy(1, 2, 3, 4), BBox.from_xyxy(4, 3, 2, 1)]
 
 
-def collate_fn(samples):
-    train_samples = [build_train_sample(**sample) for sample in samples]
-    return tuple(zip(*train_samples))
+class EfficientDet:
+    @staticmethod
+    def build_train_sample(img: np.ndarray, labels: List[int], bboxes: List[BBox]):
+        x = im2tensor(img)
+
+        y = {
+            "cls": tensor(labels, dtype=torch.int64),
+            "boxes": tensor([bbox.yxyx for bbox in bboxes], dtype=torch.float64),
+        }
+
+        return x, y
+
+    @classmethod
+    def collate_fn(cls, samples):
+        train_samples = [cls.build_train_sample(**sample) for sample in samples]
+        return tuple(zip(*train_samples))
+
+    @classmethod
+    def dataloader(cls, dataset, **kwargs):
+        return DataLoader(dataset=dataset, collate_fn=cls.collate_fn, **kwargs)
 
 
-def dataloader(dataset, **kwargs):
-    return DataLoader(dataset, collate_fn=collate_fn, **kwargs)
+def test_efficient_det_build_train_sample(img, labels, bboxes):
+    image, target = EfficientDet.build_train_sample(
+        img=img, labels=labels, bboxes=bboxes
+    )
+
+    assert torch.all(image == torch.ones((3, 4, 4), dtype=torch.float32))
+
+    assert torch.all(target["cls"] == tensor([1, 2], dtype=torch.int64))
+    assert torch.all(
+        target["boxes"] == tensor([[2, 1, 4, 3], [3, 4, 1, 2]], dtype=torch.float64)
+    )
 
 
-dataset = [{"img": img, "labels": labels, "bboxes": bboxes}] * 2
+def test_efficient_det_dataloader(img, labels, bboxes):
+    dataset = [{"img": img, "labels": labels, "bboxes": bboxes}] * 2
 
-dl = dataloader(dataset, batch_size=2)
-xb, yb = first(dl)
+    dl = EfficientDet.dataloader(dataset, batch_size=2)
+    xb, yb = first(dl)
 
-assert len(xb) == len(yb) == 2
+    assert len(xb) == len(yb) == 2
 
-for x in xb:
-    assert x.shape == (3, 4, 4)
+    for x in xb:
+        assert x.shape == (3, 4, 4)
 
-for y in yb:
-    assert set(y.keys()) == {"cls", "boxes"}
+    for y in yb:
+        assert set(y.keys()) == {"cls", "boxes"}
