73745b7d1de715643b2dc9cfbf3eea55fb7b34df
lgvaz <lucasgouvaz@gmail.com>
2020-07-12 11:30:12 -0300

    adds mobilenet param_groups support

---
+++ tests/backbones/mobilenet.py
@@ -0,0 +1,9 @@
+from mantisshrimp import *
+
+
+def test_mobilenet_param_groups():
+    model = backbones.mobilenet(pretrained=False)
+    param_groups = model.param_groups()
+
+    assert len(param_groups) == 4
+    assert check_all_model_params_in_groups2(model, param_groups)
