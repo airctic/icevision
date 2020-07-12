eec58278d07a024fc506053bbfb00f7c96a362f8
lgvaz <lucasgouvaz@gmail.com>
2020-07-11 22:17:38 -0300

    refactors resnet backbones

--- mantisshrimp/backbones/__init__.py
+++ mantisshrimp/backbones/__init__.py
@@ -1,7 +1,5 @@
-from mantisshrimp.backbones.torchvision_backbones import *
-from torchvision.models.detection.backbone_utils import (
-    BackboneWithFPN,
-    resnet_fpn_backbone,
-)
+# from mantisshrimp.backbones.torchvision_backbones import *
+
+from mantisshrimp.backbones.resnet import *
 
 import mantisshrimp.backbones.resnet_fpn
