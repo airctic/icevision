fc32bd347d8079565c032041c89789549db07b41
lgvaz <lucasgouvaz@gmail.com>
2020-07-12 11:49:24 -0300

    adds vgg param_groups

---
+++ tests/backbones/vgg.py
@@ -0,0 +1,12 @@
+import pytest
+from mantisshrimp import *
+
+
+@pytest.mark.parametrize("model_name", ["vgg11", "vgg16"])
+def test_vgg_param_groups(model_name):
+    model_fn = getattr(backbones, model_name)
+    model = model_fn(pretrained=False)
+
+    param_groups = model.param_groups()
+    assert len(param_groups) == 4
+    check_all_model_params_in_groups2(model, param_groups)
