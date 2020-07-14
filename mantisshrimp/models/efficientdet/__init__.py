d9a90a18466b33a4cf9077b2cc0181ea715a25eb
lgvaz <lucasgouvaz@gmail.com>
2020-07-14 11:31:37 -0300

    renames efficient_det -> efficientdet

---
+++ mantisshrimp/models/efficientdet/__init__.py
@@ -0,0 +1,12 @@
+from mantisshrimp.models.efficientdet.model import *
+from mantisshrimp.models.efficientdet.dataloaders import *
+from mantisshrimp.models.efficientdet.loss_fn import *
+
+# Soft dependencies
+from mantisshrimp.utils.soft_dependencies import *
+
+if HAS_FASTAI:
+    import mantisshrimp.models.efficientdet.fastai
+
+if HAS_LIGHTNING:
+    import mantisshrimp.models.efficientdet.lightning
