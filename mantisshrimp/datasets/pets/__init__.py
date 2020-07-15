1de8fc5cd47c15065374df9884e7d1e05e0e2ae1
AI Fast Track <59614262+ai-fast-track@users.noreply.github.com>
2020-07-15 17:28:57 -0400

    added transforms for PETS dataset in pets folder (#178)
    * adde transforms for PETS dataset in pets folder
    
    * split method into 2, and move import
    
    * updated calls
    
    * moved Albumentations import inside the methods
    
    * add methods to __all__
    
    * fixed old name used in examples

--- mantisshrimp/datasets/pets/__init__.py
+++ mantisshrimp/datasets/pets/__init__.py
@@ -1,3 +1,4 @@
 from mantisshrimp.datasets.pets.data import *
 from mantisshrimp.datasets.pets.parsers import *
 from mantisshrimp.datasets.pets.visualization import *
+from mantisshrimp.datasets.pets.transforms import *
