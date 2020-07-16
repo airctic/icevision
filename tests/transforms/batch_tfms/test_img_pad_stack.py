0d61a7efd8383f60721473edfc143e657f39bec8
lgvaz <lucasgouvaz@gmail.com>
2020-07-15 22:23:31 -0300

    adds custom pad_value to ImgPadStack

--- tests/transforms/batch_tfms/test_img_pad_stack.py
+++ tests/transforms/batch_tfms/test_img_pad_stack.py
@@ -5,15 +5,17 @@ from mantisshrimp import *
 
 @pytest.fixture()
 def records():
-    imgs = [np.ones((2, 4, 3), dtype=np.uint8), np.ones((3, 2, 3), dtype=np.uint8)]
+    imgs = [np.ones((2, 4, 3), dtype=np.float), np.ones((3, 2, 3), dtype=np.float)]
     return [{"img": img} for img in imgs]
 
 
-def test_img_pad_stack(records):
-    tfmed_records = batch_tfms.ImgPadStack()(records)
-
+@pytest.mark.parametrize("pad_value", [0, (1, 2, 3)])
+def test_img_pad_stack(records, pad_value):
+    tfmed_records = batch_tfms.ImgPadStack(pad_value=pad_value)(records)
     imgs = np.asarray([record["img"] for record in tfmed_records])
-    expected = np.zeros((2, 3, 4, 3))
+
+    expected = np.ones((2, 3, 4, 3), dtype=np.float)
+    expected *= np.array(pad_value).reshape(-1)
     expected[0, :2, :4, :3] = 1
     expected[1, :3, :2, :3] = 1
     np.testing.assert_equal(imgs, expected)
