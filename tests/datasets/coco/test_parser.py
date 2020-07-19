0707e0dfd22b00953ae5d1ad503699e71345c4ff
lgvaz <lucasgouvaz@gmail.com>
2020-07-18 21:09:24 -0300

    adds helper function for coco_parser

---
+++ tests/datasets/coco/test_parser.py
@@ -0,0 +1,21 @@
+import pytest
+from mantisshrimp import *
+
+
+def test_coco_parser(samples_source):
+    parser = datasets.coco.parser(
+        annotations_file=samples_source / "annotations.json",
+        img_dir=samples_source / "images",
+    )
+
+    records = parser.parse()[0]
+    assert len(records) == 5
+
+    record = records[0]
+    assert record["imageid"] == 0
+    assert record["filepath"] == samples_source / "images/000000128372.jpg"
+    assert record["height"] == 427
+    assert record["width"] == 640
+
+    assert record["labels"] == [6, 1, 1, 1, 1, 1, 1, 1, 31, 31, 1, 3, 31, 1, 31, 31]
+    assert pytest.approx(record["bboxes"][0].xywh) == [0, 73.89, 416.44, 305.13]
