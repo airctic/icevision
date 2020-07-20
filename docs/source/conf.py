586d6e77a20cfa00301e5dbd1b03ca20e65d4be4
Aditya Oke <47158509+oke-aditya@users.noreply.github.com>
2020-07-21 00:32:16 +0530

    Tries shifting to pytorch theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,12 +50,22 @@ source_suffix = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "pytorch_sphinx_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# CSS Path for docs
+html_css_files = [
+    "css/theme.css",
+]
+
+# JS path for js stuff
+html_js_files = [
+    "js/custom.js",
+]
 
 # Add type of source files
 source_suffix = [".rst", ".md", ".ipynb"]
