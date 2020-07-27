0217f503108041941e3dff2a14149101b48d6604
Aditya Oke <47158509+oke-aditya@users.noreply.github.com>
2020-07-27 18:21:57 +0530

    Removes pandas

--- mantisshrimp/imports.py
+++ mantisshrimp/imports.py
@@ -4,7 +4,6 @@ from fastcore.imports import *
 from fastcore.foundation import *
 from fastcore.utils import *
 
-import pandas as pd
 import torch.nn as nn
 import pytorch_lightning as pl
 import torch.optim.lr_scheduler as lr_scheduler
