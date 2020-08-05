7163332bc4549c466c7fbe6e2fe5a43b3d214e8c
lgvaz <lucasgouvaz@gmail.com>
2020-08-05 15:31:19 -0300

    fixes python3.6 import issue

--- mantisshrimp/tfms/__init__.py
+++ mantisshrimp/tfms/__init__.py
@@ -5,4 +5,6 @@ import mantisshrimp.tfms.batch
 from mantisshrimp.utils.soft_dependencies import HAS_ALBUMENTATIONS
 
 if HAS_ALBUMENTATIONS:
-    import mantisshrimp.tfms.albumentations as A
+    # `import mantisshrimp.tfms.albumentations as A` fails in python 3.6
+    # https://stackoverflow.com/a/24968941/6772672
+    from mantisshrimp.tfms import albumentations as A
