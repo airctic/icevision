65efb8e195f391eb6fb845a87786d76564fd76ac
Lucas Vazquez <lucasgouvaz@gmail.com>
2020-08-07 20:43:18 -0300

    adds docs for dataset, metrics and splits (#284)
    * adds docs for dataset, metrics and splits
    
    * fixes formatting

--- mantisshrimp/parsers/splits.py
+++ mantisshrimp/parsers/splits.py
@@ -9,25 +9,55 @@ from mantisshrimp.utils import *
 
 
 class DataSplitter(ABC):
+    """Base class for all data splitters.
+    """
+
     def __call__(self, ids):
         return self.split(ids)
 
     @abstractmethod
     def split(self, ids):
+        """Splits `ids` into groups.
+
+        # Arguments
+            ids: Items to be splitted.
+        """
         pass
 
 
 class SingleSplitSplitter(DataSplitter):
+    """Return all items in a single group, without shuffling.
+    """
+
     def split(self, ids):
+        """Puts all `ids` in a single group.
+
+        # Arguments
+            ids: Items to be splitted.
+        """
         return [ids]
 
 
 class RandomSplitter(DataSplitter):
-    def __init__(self, probs, seed=None):
+    """Randomly splits items.
+
+    # Arguments
+        probs: `Sequence` of probabilities that must sum to one. The length of the 
+            `Sequence` is the number of groups to to split the items into.
+        seed: Internal seed used for shuffling the items. Define this if you need
+            reproducible results.
+    """
+
+    def __init__(self, probs: Sequence[int], seed: int = None):
         self.probs = probs
         self.seed = seed
 
     def split(self, ids):
+        """Randomly splits `ids` based on parameters passed to the constructor of this class.
+
+        # Arguments
+            ids: Items to be splitted.
+        """
         # calculate split indexes
         p = np.array(self.probs) * len(ids)  # convert percentage to absolute
         p = np.ceil(p).astype(int)  # round up, so each split has at least one example
