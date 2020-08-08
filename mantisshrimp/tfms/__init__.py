5c7e63c7a66f69e749874d13ce34b174d90937af
lgvaz <lucasgouvaz@gmail.com>
2020-08-07 21:29:42 -0300

    refactors soft_dependencies

--- mantisshrimp/tfms/__init__.py
+++ mantisshrimp/tfms/__init__.py
@@ -2,9 +2,9 @@ from mantisshrimp.tfms.transform import *
 
 import mantisshrimp.tfms.batch
 
-from mantisshrimp.utils.soft_dependencies import HAS_ALBUMENTATIONS
+from mantisshrimp.utils import SoftDependencies
 
-if HAS_ALBUMENTATIONS:
+if SoftDependencies.albumentations:
     # `import mantisshrimp.tfms.albumentations as A` fails in python 3.6
     # https://stackoverflow.com/a/24968941/6772672
     from mantisshrimp.tfms import albumentations as A
