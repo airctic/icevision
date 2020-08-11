84276f527412c09be5497847f47a6bd952e4d9d2
ai-fast-track <farid@itecpedia.com>
2020-08-10 22:15:12 -0400

    removed pets.transforms (not used anymore)

--- mantisshrimp/datasets/pets/__init__.py
+++ mantisshrimp/datasets/pets/__init__.py
@@ -1,4 +1,3 @@
 from mantisshrimp.datasets.pets.data import *
 from mantisshrimp.datasets.pets.parsers import *
 from mantisshrimp.datasets.pets.visualization import *
-from mantisshrimp.datasets.pets.transforms import *
