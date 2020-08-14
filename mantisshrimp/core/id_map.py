d2a24bfc883db6b5c0516499d96ab5691b75aebd
Lucas Vazquez <lucasgouvaz@gmail.com>
2020-08-14 19:05:34 -0300

    Implements FixedSplitter, refactors IDMap and DataSplitter (#319)

--- mantisshrimp/core/id_map.py
+++ mantisshrimp/core/id_map.py
@@ -8,15 +8,38 @@ class IDMap:
     Works like a dictionary that automatically assign values for new keys.
     """
 
-    def __init__(self, imageids=None):
-        self.i2imageid = imageids or []
-        self.imageid2i = OrderedDict(enumerate(self.i2imageid))
+    def __init__(self, initial_names: Optional[Sequence[Hashable]] = None):
+        names = initial_names or []
+        self.id2name = OrderedDict((id, name) for id, name in enumerate(names))
+        self.name2id = OrderedDict((name, id) for id, name in enumerate(names))
+
+    def get_id(self, id: int) -> Hashable:
+        return self.id2name[id]
+
+    def get_name(self, name: Hashable) -> int:
+        try:
+            id = self.name2id[name]
+        except KeyError:
+            id = len(self.name2id)
+            self.name2id[name] = id
+            self.id2name[id] = name
+
+        return id
+
+    def filter_ids(self, ids: List[int]) -> "IDMap":
+        idmap = IDMap()
+        for id in ids:
+            name = self.get_id(id)
+            idmap.id2name[id] = name
+            idmap.name2id[name] = id
+
+        return idmap
+
+    def get_ids(self) -> List[int]:
+        return list(self.id2name.keys())
+
+    def get_names(self) -> List[Hashable]:
+        return list(self.name2id.keys())
 
     def __getitem__(self, imageid):
-        try:
-            i = self.imageid2i[imageid]
-        except KeyError:
-            i = len(self.i2imageid)
-            self.imageid2i[imageid] = i
-            self.i2imageid.append(imageid)
-        return i
+        return self.get_name(imageid)
--- mantisshrimp/data/__init__.py
+++ mantisshrimp/data/__init__.py
@@ -1,3 +1,4 @@
 from mantisshrimp.data.prepare_record import *
+from mantisshrimp.data.data_splitter import *
 from mantisshrimp.data.dataset import *
 from mantisshrimp.data.convert_records_to_coco_style import *
