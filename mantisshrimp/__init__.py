bb507ec973b19d86fe23848afea375324afed1a3
Lucas Vazquez <lucasgouvaz@gmail.com>
2020-08-16 21:33:14 -0300

    only uses from imports (#330)

--- mantisshrimp/__init__.py
+++ mantisshrimp/__init__.py
@@ -1,11 +1,11 @@
 from mantisshrimp.utils import *
 from mantisshrimp.core import *
 from mantisshrimp import parsers
-import mantisshrimp.tfms as tfms
+from mantisshrimp import tfms
 from mantisshrimp.data import *
-import mantisshrimp.datasets as datasets
-import mantisshrimp.backbones
-import mantisshrimp.models
+from mantisshrimp import datasets
+from mantisshrimp import backbones
+from mantisshrimp import models
 from mantisshrimp.models import *
 from mantisshrimp.metrics import *
 from mantisshrimp.visualize import *
