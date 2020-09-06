6f192478bf64a71b97ffcdfa396e8823901ed491
lgvaz <lucasgouvaz@gmail.com>
2020-09-06 10:57:30 -0300

    implements show results

--- icevision/models/efficientdet/__init__.py
+++ icevision/models/efficientdet/__init__.py
@@ -2,6 +2,7 @@ from icevision.models.efficientdet.model import *
 from icevision.models.efficientdet.dataloaders import *
 from icevision.models.efficientdet.loss_fn import *
 from icevision.models.efficientdet.prediction import *
+from icevision.models.efficientdet.show_results import *
 
 # Soft dependencies
 from icevision.soft_dependencies import SoftDependencies
