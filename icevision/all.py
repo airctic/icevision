579d96a038391e6c858846e3174ac7a0758ed63d
lgvaz <lgvaz42@gmail.com>
2020-09-23 22:35:14 -0300

    soft import icedata in icevision.all

--- icevision/all.py
+++ icevision/all.py
@@ -1,2 +1,9 @@
 from icevision.imports import *
 from icevision import *
+
+# soft import icedata
+try:
+    import icedata
+except ModuleNotFoundError as e:
+    if str(e) != f"No module named icedata":
+        raise e
