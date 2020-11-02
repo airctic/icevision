49ab4892a87753bd4895178d5cbdbb0551276ebe
lgvaz <lgvaz42@gmail.com>
2020-11-02 16:04:09 -0300

    fixes pretrained in rcnn models

--- icevision/models/rcnn/faster_rcnn/model.py
+++ icevision/models/rcnn/faster_rcnn/model.py
@@ -14,7 +14,6 @@ def model(
     backbone: Optional[nn.Module] = None,
     remove_internal_transforms: bool = True,
     pretrained: bool = True,
-    pretrained_backbone=True,
     **faster_rcnn_kwargs
 ) -> nn.Module:
     """FasterRCNN model implemented by torchvision.
@@ -25,10 +24,9 @@ def model(
         remove_internal_transforms: The torchvision model internally applies transforms
         like resizing and normalization, but we already do this at the `Dataset` level,
         so it's safe to remove those internal transforms.
-        pretrained: Argument passed to `maskrcnn_resnet50_fpn` if `backbone is None`.
+        pretrained: Argument passed to `fastercnn_resnet50_fpn` if `backbone is None`.
         By default it is set to True: this is generally used when training a new model (transfer learning).
         `pretrained = False`  is used during inference (prediction) for cases where the users have their own pretrained weights.
-        `pretrained_backbone = False`  is used during inference (prediction) for cases where the users have their own pretrained backbone weights.
         **faster_rcnn_kwargs: Keyword arguments that internally are going to be passed to
         `torchvision.models.detection.faster_rcnn.FastRCNN`.
 
@@ -37,9 +35,7 @@ def model(
     """
     if backbone is None:
         model = fasterrcnn_resnet50_fpn(
-            pretrained=True,
-            pretrained_backbone=pretrained_backbone,
-            **faster_rcnn_kwargs
+            pretrained=pretrained, pretrained_backbone=pretrained, **faster_rcnn_kwargs
         )
 
         in_features = model.roi_heads.box_predictor.cls_score.in_features
