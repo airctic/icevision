5aa91d5c974029cb080cbae25a54687248e52ddb
AI Fast Track <59614262+ai-fast-track@users.noreply.github.com>
2020-11-02 10:45:43 -0500

    Added pretrained_backbone arg to model() (#520)
    * releases 0.2.2
    
    * add pretrained_backbone arg to mask-rcnn model()
    
    * added pretrained_backbone arg to model()
    
    * added PR #
    
    Co-authored-by: lgvaz <lgvaz42@gmail.com>

--- icevision/models/rcnn/mask_rcnn/model.py
+++ icevision/models/rcnn/mask_rcnn/model.py
@@ -15,6 +15,7 @@ def model(
     backbone: Optional[nn.Module] = None,
     remove_internal_transforms: bool = True,
     pretrained: bool = True,
+    pretrained_backbone=True,
     **mask_rcnn_kwargs
 ) -> nn.Module:
     """MaskRCNN model implemented by torchvision.
@@ -28,6 +29,7 @@ def model(
         pretrained: Argument passed to `maskrcnn_resnet50_fpn` if `backbone is None`.
         By default it is set to True: this is generally used when training a new model (transfer learning).
         `pretrained = False`  is used during inference (prediction) for cases where the users have their own pretrained weights.
+        `pretrained_backbone = False`  is used during inference (prediction) for cases where the users have their own pretrained backbone weights.
         **mask_rcnn_kwargs: Keyword arguments that internally are going to be passed to
         `torchvision.models.detection.mask_rcnn.MaskRCNN`.
 
@@ -35,7 +37,11 @@ def model(
         A Pytorch `nn.Module`.
     """
     if backbone is None:
-        model = maskrcnn_resnet50_fpn(pretrained=pretrained, **mask_rcnn_kwargs)
+        model = maskrcnn_resnet50_fpn(
+            pretrained=pretrained,
+            pretrained_backbone=pretrained_backbone,
+            **mask_rcnn_kwargs
+        )
 
         in_features_box = model.roi_heads.box_predictor.cls_score.in_features
         model.roi_heads.box_predictor = FastRCNNPredictor(in_features_box, num_classes)
