03573cee7fdf5fc265a304b9ef5713d436e1bcb8
FraPochetti <fra.pochetti@gmail.com>
2020-11-06 22:11:26 +0100

    added COCOCOCOKeyPointsParser

--- tests/parsers/test_coco_parser.py
+++ tests/parsers/test_coco_parser.py
@@ -12,14 +12,21 @@ def coco_mask_parser(coco_dir):
     return parsers.coco(coco_dir / "annotations.json", coco_dir / "images", mask=True)
 
 
+@pytest.fixture
+def coco_keypoints_parser(coco_dir):
+    return parsers.COCOKeyPointsParser(
+        coco_dir / "keypoints_annotations.json", coco_dir / "images"
+    )
+
+
 def test_keypoints_parser(coco_dir, coco_keypoints_parser):
-    records = coco_keypoints_parser.parse(data_splitter=SingleSplitSplitter())[0]
-    assert len(records) == 2
-    assert records[1].filepath == coco_dir / "images/000000404249.jpg"
-    assert len(records[1].keypoints) == 1
-    assert records[1].keypoints[0].n_visible_keypoints == 16
-    assert records[1].keypoints[0].y.max() == 485
-    assert len(records[0].keypoints) == 9
+    records = coco_keypoints_parser.parse(data_splitter=SingleSplitSplitter())
+    assert len(records[0]) == 2
+    assert records[0][1].filepath == coco_dir / "images/000000404249.jpg"
+    assert len(records[0][1].keypoints) == 1
+    assert records[0][1].keypoints[0].n_visible_keypoints == 16
+    assert records[0][1].keypoints[0].y.max() == 485
+    assert len(records[0][0].keypoints) == 9
 
 
 def test_bbox_parser(coco_dir, coco_bbox_parser):
