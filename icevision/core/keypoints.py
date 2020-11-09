18bf54d58505edb5e4a6e8b3f441398503c65ca5
FraPochetti <fra.pochetti@gmail.com>
2020-11-09 12:31:33 +0100

    added Dataset functionality, e.g. albumentations with keypoints

--- icevision/core/keypoints.py
+++ icevision/core/keypoints.py
@@ -10,10 +10,11 @@ class KeyPoints:
     """
 
     def __init__(self, keypoints: Union[List[int], np.array]):
-        self.keypoints = np.array(keypoints)
+        self.keypoints = np.array([int(round(k)) for k in keypoints])
         self.x = self.keypoints[0::3]
         self.y = self.keypoints[1::3]
         self.visible = self.keypoints[2::3]
+        self.xy = [(x, y) for x, y in zip(self.x, self.y)]
         self.n_visible_keypoints = (self.visible > 0).sum()
 
         self.human_kps = [
