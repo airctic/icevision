1cd0a17a8591de52fc275d4b66b105092bc01ea0
FraPochetti <fra.pochetti@gmail.com>
2020-12-18 09:02:15 +0100

    refactoring plot_top_losses logic

--- icevision/models/torchvision_models/faster_rcnn/show_results.py
+++ icevision/models/torchvision_models/faster_rcnn/show_results.py
@@ -1,4 +1,4 @@
-__all__ = ["show_results", "get_losses", "plot_top_losses", "add_annotations"]
+__all__ = ["show_results", "interp"]
 
 from icevision.imports import *
 from icevision.utils import *
@@ -14,7 +14,7 @@ from icevision.models.torchvision_models.faster_rcnn.prediction import (
     predict,
     predict_dl,
 )
-from icevision.visualize.show_data import show_preds
+from icevision.models.interpretation import Interpretation
 
 
 def show_results(
@@ -39,129 +39,17 @@ def show_results(
     )
 
 
-def _move_to_device(x, y, device):
-    x = [o.to(device) for o in x]
-    y = [
-        {k: (v.to(device) if isinstance(v, torch.Tensor) else v) for k, v in o.items()}
-        for o in y
-    ]
-    return x, y
+_LOSSES_DICT = {
+    "loss_classifier": [],
+    "loss_box_reg": [],
+    "loss_objectness": [],
+    "loss_rpn_box_reg": [],
+    "loss_total": [],
+}
 
-
-def get_losses(
-    model: nn.Module,
-    dataset: Dataset,
-) -> Tuple[List[dict], dict]:
-    """
-    Gets a dataset and a model as input and returns losses calculated per image + losses stats.
-    In case of faster_rcnn those are loss_classifier, loss_box_reg and loss_total
-    (sum of the first 2 plus RPN related losses, e.g. sum of 4 terms).
-
-    Arguments
-    ---------
-    model: nn.Module
-    dataset: Dataset
-
-    Returns
-    -------
-    samples_plus_losses: those are the samples (dictionaries) contained in the original dataset
-                          enriched with losses.
-    losses_stats: dictionary containing losses stats (min, max, mean, 1-25-50-75-99 quantiles)
-                  for each one of the losses.
-    """
-
-    device = model_device(model)
-    dl = valid_dl(dataset, batch_size=1, num_workers=0, shuffle=False)
-
-    samples_plus_losses = []
-    losses_stats = {
-        "loss_classifier": [],
-        "loss_box_reg": [],
-        "loss_objectness": [],
-        "loss_rpn_box_reg": [],
-        "loss_total": [],
-    }
-
-    with torch.no_grad():
-        for (x, y), sample in pbar(dl):
-            torch.manual_seed(0)
-            x, y = _move_to_device(x, y, device)
-            loss = model(x, y)
-            loss = {k: float(v.cpu().numpy()) for k, v in loss.items()}
-            loss["loss_total"] = sum(loss.values())
-
-            for l in losses_stats.keys():
-                losses_stats[l].append(loss[l])
-
-            sample[0].update(loss)
-            samples_plus_losses.append(sample[0])
-
-    losses_stats = {k: get_stats(v) for k, v in losses_stats.items()}
-    return samples_plus_losses, losses_stats
-
-
-def add_annotations(samples: List[dict]) -> List[dict]:
-    """
-    Adds a `text` field to the sample dict to use as annotations when plotting.
-    """
-    for sample in samples:
-        text = ""
-        for key in sample.keys():
-            if "loss" in key:
-                text += f"{key}: {round(sample[key], 5)}\n"
-        text += f"IMG: {sample['filepath'].name}"
-        sample["text"] = text
-    return samples
-
-
-def plot_top_losses(
-    model: nn.Module,
-    dataset: Dataset,
-    sort_by: str = "loss_total",
-    n_samples: int = 5,
-    batch_size: int = 8,
-) -> Tuple[List[dict], List[dict], dict]:
-    """
-    Gets a dataset and a model as input. Calculates losses for each sample in the dataset.
-    Sorts samples by `sort_by` (e.g. one of the losses). Runs model predictions on samples.
-    Plots the top sorted `n_samples` and returns losses + predictions + losses_stats.
-
-    Arguments
-    ---------
-    model: nn.Module
-    dataset: Dataset
-    sort_by: (str) the loss to sort samples by
-    n_samples: how many samples to show
-    batch_size: used when creating the infer dataloader to get model predictions on the dataset
-
-    Returns
-    -------
-    sorted_samples: those are the samples (dictionaries) contained in the original dataset
-                    enriched with losses and sorted by `sort_by`.
-    sorted_preds: `model` predictions on `sorted_samples`. Same order as `sorted_samples`.
-    losses_stats: dictionary containing losses stats (min, max, mean, 1-25-50-75-99 quantiles)
-                  for each one of the losses.
-    """
-    samples, losses_stats = get_losses(model, dataset)
-    samples = add_annotations(samples)
-
-    dl = infer_dl(dataset, batch_size=batch_size)
-    _, preds = predict_dl(model=model, infer_dl=dl)
-
-    sorted_samples, sorted_preds, annotations = sort_losses(samples, preds, by=sort_by)
-    assert len(sorted_samples) == len(samples) == len(preds) == len(sorted_preds)
-
-    anns = []
-    for ann in annotations:
-        ann = ann.split("\n")
-        ann1 = "\n".join(ann[:3])
-        ann2 = "\n".join(ann[3:])
-        anns.append((ann1, ann2))
-
-    show_preds(
-        samples=sorted_samples[:n_samples],
-        preds=sorted_preds[:n_samples],
-        annotations=anns[:n_samples],
-    )
-    model.train()
-    return sorted_samples, sorted_preds, losses_stats
+interp = Interpretation(
+    losses_dict=_LOSSES_DICT,
+    valid_dl=valid_dl,
+    infer_dl=infer_dl,
+    predict_dl=predict_dl,
+)
--- icevision/utils/utils.py
+++ icevision/utils/utils.py
@@ -15,9 +15,6 @@ __all__ = [
     "normalize_imagenet",
     "denormalize_imagenet",
     "patch_class_to_main",
-    "sort_losses",
-    "get_stats",
-    "compute_weighted_sum",
 ]
 
 from icevision.imports import *
@@ -106,69 +103,3 @@ def patch_class_to_main(cls):
     setattr(__main__, cls.__name__, cls)
     cls.__module__ = "__main__"
     return cls
-
-
-def compute_weighted_sum(sample, weights):
-    loss_weighted = 0
-    for loss, weight in weights.items():
-        loss_weighted += sample[loss] * weight
-    sample["loss_weighted"] = loss_weighted
-    return sample
-
-
-def sort_losses(
-    samples: List[dict], preds: List[dict], by: Union[str, dict] = "loss_total"
-) -> Tuple[List[dict], List[dict], List[str]]:
-    by_copy = deepcopy(by)
-    losses_expected = [
-        k for k in samples[0].keys() if "loss" in k and k != "loss_total"
-    ]
-
-    if isinstance(by, str):
-        loss_check = losses_expected + ["loss_total"]
-        assert (
-            by in loss_check
-        ), f"You must `sort_by` one of the losses. '{by}' is not among {loss_check}"
-
-    if isinstance(by, dict):
-        expected = ["weighted"]
-        assert (
-            by["method"] in expected
-        ), f"`method` must be in {expected}, got {by['method']} instead."
-        if by["method"] == "weighted":
-            losses_passed = set(by["weights"].keys())
-            losses_expected = set(losses_expected)
-            assert (
-                losses_passed == losses_expected
-            ), f"You need to pass a weight for each of the losses in {losses_expected}, got {losses_passed} instead."
-            samples = [compute_weighted_sum(s, by["weights"]) for s in samples]
-            by = "loss_weighted"
-
-    l = list(zip(samples, preds))
-    l = sorted(l, key=lambda i: i[0][by], reverse=True)
-    sorted_samples, sorted_preds = zip(*l)
-    annotations = [el["text"] for el in sorted_samples]
-
-    if isinstance(by_copy, dict):
-        if by_copy["method"] == "weighted":
-            annotations = [
-                f"loss_weighted: {round(s['loss_weighted'], 5)}\n" + a
-                for a, s in zip(annotations, sorted_samples)
-            ]
-
-    return list(sorted_samples), list(sorted_preds), annotations
-
-
-def get_stats(l: List) -> dict:
-    l = np.array(l)
-    quants_names = ["1ile", "25ile", "50ile", "75ile", "99ile"]
-    quants = np.quantile(l, [0.01, 0.25, 0.5, 0.75, 0.99])
-    d = {
-        "min": l.min(),
-        "max": l.max(),
-        "mean": l.mean(),
-    }
-
-    q = {k: v for k, v in zip(quants_names, quants)}
-    d.update(q)
-    return d
