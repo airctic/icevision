9f5542a41d4f8b03ae2962e6e314d1cd4b4b9f04
lgvaz <lgvaz42@gmail.com>
2021-01-08 09:31:40 -0300

    mmdetection first sketch

---
+++ icevision/models/mmdetection_models/__init__.py
@@ -0,0 +1,2 @@
+from icevision.models.mmdetection_models.dataloaders import *
+from icevision.models.mmdetection_models.lightning import *
---
+++ icevision/models/mmdetection_models/lightning/__init__.py
@@ -0,0 +1,1 @@
+from icevision.models.mmdetection_models.lightning.model_adapter import *
