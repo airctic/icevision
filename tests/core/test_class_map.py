52628ccd29c4cf773f4b40a26f1da385aa5ed320
lgvaz <lgvaz42@gmail.com>
2021-01-20 21:44:27 -0300

    fixes class_map background to always 0

--- tests/core/test_class_map.py
+++ tests/core/test_class_map.py
@@ -13,18 +13,13 @@ def test_class_map_no_background(classes):
     assert class_map.get_name("a") == 0
 
 
-def test_class_map_first_background(classes):
-    class_map = ClassMap(classes, background=0)
-    assert class_map.get_id(0) == "background"
+@pytest.mark.parametrize("background", [BACKGROUND, "bg", 0])
+def test_class_map_background(classes, background):
+    class_map = ClassMap(classes, background=background)
+    assert class_map.get_id(0) == background
+    assert class_map.get_name(background) == 0
     assert class_map.get_id(1) == "a"
     assert class_map.get_name("a") == 1
-
-
-def test_class_map_last_background(classes):
-    class_map = ClassMap(classes, background=-1)
-    assert class_map.get_id(-1) == "background"
-    assert class_map.get_id(0) == "a"
-    assert class_map.get_name("a") == 0
 
 
 def test_class_map_safety(classes):
