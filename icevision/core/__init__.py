ba857ed0afedde5aaf69105cc68cf2a86e3d55b4
lgvaz <lgvaz42@gmail.com>
2021-02-08 10:56:12 -0300

    removes record_mixin

--- icevision/core/__init__.py
+++ icevision/core/__init__.py
@@ -5,7 +5,6 @@ from icevision.core.class_map import *
 from icevision.core.mask import *
 from icevision.core.bbox import *
 from icevision.core.record_type import *
-from icevision.core.record_mixins import *
 from icevision.core.record_components import *
 from icevision.core.record import *
 from icevision.core.keypoints import *
