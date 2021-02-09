aa9bbd05a655f8a649f37a145db86ac8f4018d73
FraPochetti <fra.pochetti@gmail.com>
2021-02-09 17:55:37 +0100

    fixing formatting

--- samples/_base_/default_runtime.py
+++ samples/_base_/default_runtime.py
@@ -3,14 +3,15 @@ checkpoint_config = dict(interval=1)
 log_config = dict(
     interval=50,
     hooks=[
-        dict(type='TextLoggerHook'),
+        dict(type="TextLoggerHook"),
         # dict(type='TensorboardLoggerHook')
-    ])
+    ],
+)
 # yapf:enable
-custom_hooks = [dict(type='NumClassCheckHook')]
+custom_hooks = [dict(type="NumClassCheckHook")]
 
-dist_params = dict(backend='nccl')
-log_level = 'INFO'
+dist_params = dict(backend="nccl")
+log_level = "INFO"
 load_from = None
 resume_from = None
-workflow = [('train', 1)]
+workflow = [("train", 1)]
--- tests/models/mmdet/test_model.py
+++ tests/models/mmdet/test_model.py
@@ -28,39 +28,44 @@ from icevision.all import *
         ),
     ],
 )
-class TestBboxModels:  
-    def test_mmdet_bbox_models_fastai(self, ds, model_type, path, config, weights_path, request):
+class TestBboxModels:
+    def test_mmdet_bbox_models_fastai(
+        self, ds, model_type, path, config, weights_path, request
+    ):
         train_ds, valid_ds = request.getfixturevalue(ds)
         train_dl = model_type.train_dl(train_ds, batch_size=2)
         valid_dl = model_type.valid_dl(valid_ds, batch_size=2)
-        
+
         config_path = request.getfixturevalue(path) / config
-        
+
         model = model_type.model(config_path, num_classes=5, weights_path=weights_path)
 
-        learn = model_type.fastai.learner(dls=[train_dl, valid_dl], model=model, splitter=fastai.trainable_params)
+        learn = model_type.fastai.learner(
+            dls=[train_dl, valid_dl], model=model, splitter=fastai.trainable_params
+        )
         learn.fine_tune(1, 3e-4)
 
-
-    def test_mmdet_bbox_models_light(self, ds, model_type, path, config, weights_path, request):
+    def test_mmdet_bbox_models_light(
+        self, ds, model_type, path, config, weights_path, request
+    ):
         train_ds, valid_ds = request.getfixturevalue(ds)
         train_dl = model_type.train_dl(train_ds, batch_size=2)
         valid_dl = model_type.valid_dl(valid_ds, batch_size=2)
-        
+
         config_path = request.getfixturevalue(path) / config
-        
+
         model = model_type.model(config_path, num_classes=5, weights_path=weights_path)
 
         class LitModel(model_type.lightning.ModelAdapter):
             def configure_optimizers(self):
                 return Adam(self.parameters(), lr=1e-4)
-    
+
         light_model = LitModel(model)
         trainer = pl.Trainer(
-                max_epochs=1,
-                weights_summary=None,
-                num_sanity_val_steps=0,
-                logger=False,
-                checkpoint_callback=False,
-            )
+            max_epochs=1,
+            weights_summary=None,
+            num_sanity_val_steps=0,
+            logger=False,
+            checkpoint_callback=False,
+        )
         trainer.fit(light_model, train_dl, valid_dl)
