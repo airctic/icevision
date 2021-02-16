956635fcf5d406c576cbbe706f30d8fd1d85f46f
FraPochetti <fra.pochetti@gmail.com>
2021-02-16 08:55:44 +0100

    adding support for mask_rcnn

--- icevision/models/mmdet/common/bbox/show_results.py
+++ icevision/models/mmdet/common/bbox/show_results.py
@@ -8,6 +8,10 @@ from icevision.models.base_show_results import base_show_results
 from icevision.models.mmdet.common.bbox.dataloaders import *
 from icevision.models.mmdet.common.bbox.prediction import *
 from icevision.models.interpretation import Interpretation, _move_to_device
+from icevision.models.mmdet.common.interpretation_utils import (
+    sum_losses_mmdet,
+    loop_mmdet,
+)
 
 
 def show_results(
@@ -44,39 +48,6 @@ _LOSSES_DICT = {
     "loss_total": [],
 }
 
-
-def _sum_losses(losses_dict):
-    loss_ = {}
-    for k, v in losses_dict.items():
-        if k.startswith("loss"):
-            if isinstance(v, torch.Tensor):
-                loss_[k] = float(v.cpu().numpy())
-            elif isinstance(v, list):
-                loss_[k] = sum([float(o.cpu().numpy()) for o in v])
-
-    loss_["loss_total"] = sum(loss_.values())
-    return loss_
-
-
-def _loop_mmdet_bbox(dl, model, losses_stats, device):
-    samples_plus_losses = []
-
-    with torch.no_grad():
-        for data, sample in pbar(dl):
-            torch.manual_seed(0)
-            _, data = _move_to_device(None, data, device)
-            loss = model(**data)
-            loss = _sum_losses(loss)
-
-            for l in losses_stats.keys():
-                losses_stats[l].append(loss[l])
-
-            sample[0].update(loss)
-            samples_plus_losses.append(sample[0])
-
-    return samples_plus_losses, losses_stats
-
-
 interp = Interpretation(
     losses_dict=_LOSSES_DICT,
     valid_dl=valid_dl,
@@ -84,4 +55,4 @@ interp = Interpretation(
     predict_dl=predict_dl,
 )
 
-interp._loop = _loop_mmdet_bbox
+interp._loop = loop_mmdet
