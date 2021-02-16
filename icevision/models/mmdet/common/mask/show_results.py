f93c6719e78dd59617c37c6988aac951964776cb
FraPochetti <fra.pochetti@gmail.com>
2021-02-16 22:58:56 +0100

    adding documentation in jupyter notebook

--- icevision/models/mmdet/common/mask/show_results.py
+++ icevision/models/mmdet/common/mask/show_results.py
@@ -1,4 +1,4 @@
-__all__ = ["show_results", "interp"]
+__all__ = ["show_results"]
 
 from icevision.imports import *
 from icevision.utils import *
@@ -7,11 +7,6 @@ from icevision.data import *
 from icevision.models.base_show_results import base_show_results
 from icevision.models.mmdet.common.mask.dataloaders import *
 from icevision.models.mmdet.common.mask.prediction import *
-from icevision.models.interpretation import Interpretation
-from icevision.models.mmdet.common.interpretation_utils import (
-    sum_losses_mmdet,
-    loop_mmdet,
-)
 
 
 def show_results(
@@ -38,22 +33,3 @@ def show_results(
         detection_threshold=detection_threshold,
         device=device,
     )
-
-
-_LOSSES_DICT = {
-    "loss_rpn_cls": [],
-    "loss_rpn_bbox": [],
-    "loss_cls": [],
-    "loss_bbox": [],
-    "loss_mask": [],
-    "loss_total": [],
-}
-
-interp = Interpretation(
-    losses_dict=_LOSSES_DICT,
-    valid_dl=valid_dl,
-    infer_dl=infer_dl,
-    predict_dl=predict_dl,
-)
-
-interp._loop = loop_mmdet
