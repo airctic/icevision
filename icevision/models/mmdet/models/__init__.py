ee5ec8bdd0f3bb7cd6d4eca5b7b49019405d76af
AI Fast Track <59614262+ai-fast-track@users.noreply.github.com>
2021-02-23 10:58:09 -0500

    added cornernet and centripetalnet support (#652)

--- icevision/models/mmdet/models/__init__.py
+++ icevision/models/mmdet/models/__init__.py
@@ -2,6 +2,8 @@
 from icevision.models.mmdet.models import faster_rcnn
 from icevision.models.mmdet.models import retinanet
 from icevision.models.mmdet.models import fcos
+from icevision.models.mmdet.models import cornernet
+from icevision.models.mmdet.models import centripetalnet
 
 # segmentation
 from icevision.models.mmdet.models import mask_rcnn
