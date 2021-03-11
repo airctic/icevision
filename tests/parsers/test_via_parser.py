89fa4232bbee73d3fe47f63d9c9d993a1ec60563
jerbly <jeremyblythe@gmail.com>
2021-03-10 22:25:11 -0500

    via parser for 0.7

--- tests/parsers/test_via_parser.py
+++ tests/parsers/test_via_parser.py
@@ -2,7 +2,6 @@ import pytest
 from icevision.all import *
 
 
-@pytest.mark.skip
 def test_bbox_parser(via_dir, via_bbox_class_map):
     parser = parsers.via(via_dir / "via_bbox.json", via_dir, via_bbox_class_map)
     records = parser.parse(data_splitter=SingleSplitSplitter())[0]
@@ -14,14 +13,13 @@ def test_bbox_parser(via_dir, via_bbox_class_map):
     assert record.width == 640
     assert record.height == 480
 
-    assert record.labels == [4, 1, 3, 2]
+    assert record.detect.labels == [4, 1, 3, 2]
     # Polygon
-    assert record.bboxes[0].xyxy == (104, 120, 155, 176)
+    assert record.detect.bboxes[0].xyxy == (104, 120, 155, 176)
     # Rect
-    assert record.bboxes[3].xyxy == (263, 55, 308, 103)
+    assert record.detect.bboxes[3].xyxy == (263, 55, 308, 103)
 
 
-@pytest.mark.skip
 def test_bbox_parser_alt_label(via_dir, via_bbox_class_map):
     parser = parsers.via(
         via_dir / "via_bbox.json", via_dir, via_bbox_class_map, label_field="object"
@@ -30,10 +28,9 @@ def test_bbox_parser_alt_label(via_dir, via_bbox_class_map):
     record = records[0]
     # Additional label is found when using the 'object' label_field.
     # When 'label' is used, one region is marked '###' and therefore not found in class_map.
-    assert record.labels == [4, 1, 3, 3, 2]
+    assert record.detect.labels == [4, 1, 3, 3, 2]
 
 
-@pytest.mark.skip
 def test_bbox_parser_broken_label(via_dir, via_bbox_class_map):
     parser = parsers.via(
         via_dir / "via_bbox.json", via_dir, via_bbox_class_map, label_field="qualities"
@@ -42,7 +39,6 @@ def test_bbox_parser_broken_label(via_dir, via_bbox_class_map):
         _ = parser.parse(data_splitter=SingleSplitSplitter())[0]
 
 
-@pytest.mark.skip
 def test_bbox_parser_missing_label(via_dir, via_bbox_class_map):
     parser = parsers.via(
         via_dir / "via_bbox.json", via_dir, via_bbox_class_map, label_field="MISSING"
