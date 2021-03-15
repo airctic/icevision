416550474549bc9bd3455b7d61640ce02f6241f3
Pawel Potrykus <p.potrykus@toucan-systems.pl>
2021-03-15 09:00:08 +0100

    update test for confusion matrix

--- tests/metrics/test_confusion_matrix.py
+++ tests/metrics/test_confusion_matrix.py
@@ -1,6 +1,13 @@
 import pytest
 from icevision.all import *
+from .test_coco_metric import records, preds
 
 
 def test_confusion_matrix(records, preds):
     confusion_matrix = SimpleConfusionMatrix()
+    confusion_matrix.accumulate(records, preds)
+    dummy_result = confusion_matrix.finalize()
+    cm_result = confusion_matrix.confusion_matrix
+    expected_result = np.diagflat([0, 0, 2, 1, 0])
+    assert dummy_result["dummy_value_for_fastai"] == -1
+    assert np.equal(cm_result, expected_result).all()
