38687dd144fd2a7100b97cab94acb291ce18abf8
ai-fast-track <farid@itecpedia.com>
2021-03-17 13:19:23 -0400

    add 3 retinanet backbones

---
+++ icevision/models/mmdet/models/retinanet/backbones.py
@@ -0,0 +1,17 @@
+r50_fpn_1x = {
+    "model_name": "retinanet",
+    "cfg_filepath": "mmdetection/configs/_base_/models/retinanet_r50_fpn.py",
+    "weights_url": "http://download.openmmlab.com/mmdetection/v2.0/retinanet/retinanet_r50_fpn_1x_coco/retinanet_r50_fpn_1x_coco_20200130-c2398f9e.pth",
+}
+
+r101_fpn_1x = {
+    "model_name": "retinanet",
+    "cfg_filepath": "mmdetection/configs/retinanet/retinanet_r101_fpn_1x_coco.py",
+    "weights_url": "http://download.openmmlab.com/mmdetection/v2.0/retinanet/retinanet_r101_fpn_1x_coco/retinanet_r101_fpn_1x_coco_20200130-7a93545f.pth",
+}
+
+x101_64_4d_fpn_1x = {
+    "model_name": "retinanet",
+    "cfg_filepath": "mmdetection/configs/retinanet/retinanet_x101_64x4d_fpn_1x_coco.py",
+    "weights_url": "http://download.openmmlab.com/mmdetection/v2.0/retinanet/retinanet_x101_64x4d_fpn_1x_coco/retinanet_x101_64x4d_fpn_1x_coco_20200130-366f5af1.pth",
+}
