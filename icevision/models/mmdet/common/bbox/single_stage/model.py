d87e278ec859826904e21eff803b60356cbb4cc4
ai-fast-track <farid@itecpedia.com>
2021-03-17 13:21:52 -0400

    pass backbone_info to internally handle config

--- icevision/models/mmdet/common/bbox/single_stage/model.py
+++ icevision/models/mmdet/common/bbox/single_stage/model.py
@@ -5,26 +5,49 @@ from mmcv import Config
 from mmdet.models import build_detector
 from mmcv.runner import load_checkpoint
 from icevision.models.mmdet.utils import *
+from icevision.utils.download_utils import *
 
 
 def model(
-    cfg: Union[str, Path, Config],
+    backbone_info,
     num_classes: int,
-    weights_path: Optional[Union[str, Path]] = None,
+    pretrained: bool = True,
+    checkpoints_path: Optional[Union[str, Path]] = None,
+    force_download=False,
 ) -> nn.Module:
 
-    # if `cfg` argument is a path (str, Path) create an Config object from the file
-    # otherwise cfg should be already an Config object
-    if isinstance(cfg, (str, Path)):
-        cfg = Config.fromfile(str(cfg))
+    save_dir = None
+
+    model_name = backbone_info["model_name"]
+    cfg_filepath = backbone_info["cfg_filepath"]
+    weights_url = backbone_info["weights_url"]
+
+    # download weights
+    if pretrained and weights_url:
+        if checkpoints_path:
+            save_dir = checkpoints_path.model_name
+        else:
+            save_dir = Path("checkpoints") / model_name
+        save_dir.mkdir(exist_ok=True, parents=True)
+
+        fname = Path(weights_url).name
+        weights_path = save_dir / fname
+
+        if not weights_path.exists() or force_download:
+            download_url(weights_url, weights_path)
+
+    if cfg_filepath:
+        cfg = Config.fromfile(cfg)
+    else:
+        raise RuntimeError("model must have a valid configuration file")
 
     cfg.model.bbox_head.num_classes = num_classes - 1
-    if weights_path is not None:
+    if pretrained and (weights_path is not None):
         cfg.model.pretrained = None
 
     _model = build_detector(cfg.model, cfg.get("train_cfg"), cfg.get("test_cfg"))
 
-    if weights_path is not None:
+    if pretrained and (weights_path is not None):
         load_checkpoint(_model, str(weights_path))
 
     _model.param_groups = MethodType(param_groups, _model)
