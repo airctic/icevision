ced72aeb04f28e3964e5aa161df345430361f0d4
Pawel Potrykus <p.potrykus@toucan-systems.pl>
2021-03-18 21:47:54 +0100

    updated test cause not using fridge class map anymore

--- tests/metrics/test_confusion_matrix.py
+++ tests/metrics/test_confusion_matrix.py
@@ -8,6 +8,6 @@ def test_confusion_matrix(records, preds):
     confusion_matrix.accumulate(records, preds)
     dummy_result = confusion_matrix.finalize()
     cm_result = confusion_matrix.confusion_matrix
-    expected_result = np.diagflat([0, 0, 2, 1, 0])
+    expected_result = np.diagflat([0, 0, 2, 1])
     assert dummy_result["dummy_value_for_fastai"] == -1
     assert np.equal(cm_result, expected_result).all()
