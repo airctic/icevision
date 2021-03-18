3d56fd5e91881d3a5b9d6eb800f1b4f8b8ab4eb9
ai-fast-track <farid@itecpedia.com>
2021-03-18 06:24:27 -0400

    use MMDetBackboneConfig instead of dict

--- icevision/models/mmdet/common/bbox/single_stage/model.py
+++ icevision/models/mmdet/common/bbox/single_stage/model.py
@@ -9,37 +9,36 @@ from icevision.utils.download_utils import *
 
 
 def model(
-    backbone,
+    backbone: MMDetBackboneConfig,
     num_classes: int,
     pretrained: bool = True,
-    checkpoints_path: Optional[Union[str, Path]] = None,
+    checkpoints_path: Optional[Union[str, Path]] = "checkpoints",
     force_download=False,
 ) -> nn.Module:
 
     save_dir = None
 
-    model_name = backbone["model_name"]
-    cfg_filepath = backbone["cfg_filepath"]
-    weights_url = backbone["weights_url"]
+    model_name = backbone.model_name
+    cfg_filepath = backbone.cfg_filepath
+    weights_url = backbone.weights_url
 
     # download weights
     if pretrained and weights_url:
-        if checkpoints_path:
-            save_dir = checkpoints_path.model_name
-        else:
-            save_dir = Path("checkpoints") / model_name
+        save_dir = Path("checkpoints") / model_name
         save_dir.mkdir(exist_ok=True, parents=True)
 
         fname = Path(weights_url).name
         weights_path = save_dir / fname
 
         if not weights_path.exists() or force_download:
-            download_url(weights_url, weights_path)
+            download_url(url=weights_url, save_path=str(weights_path))
 
     if cfg_filepath:
         cfg = Config.fromfile(cfg_filepath)
     else:
-        raise RuntimeError("model must have a valid configuration file")
+        raise RuntimeError(
+            "cfg_filepath is empty. Model must have a valid configuration file"
+        )
 
     cfg.model.bbox_head.num_classes = num_classes - 1
     if pretrained and (weights_path is not None):
