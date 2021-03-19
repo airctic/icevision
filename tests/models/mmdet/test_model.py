b952cb0a68f77e1150c37d56249c6722ff90ca4e
ai-fast-track <farid@itecpedia.com>
2021-03-19 16:49:46 -0400

    applied black

--- tests/models/mmdet/test_model.py
+++ tests/models/mmdet/test_model.py
@@ -19,7 +19,7 @@ class TestBboxModels:
         valid_dl = model_type.valid_dl(valid_ds, batch_size=2)
 
         backbone = model_type.backbones.r50_fpn_1x
-        backbone.cfg_url = path/backbone.cfg_url
+        backbone.cfg_url = path / backbone.cfg_url
 
         model = model_type.model(backbone=backbone, num_classes=5)
 
@@ -69,7 +69,7 @@ def mask_dls_model(coco_mask_records, samples_source):
     valid_dl = model_type.valid_dl(valid_ds, batch_size=2)
 
     backbone = model_type.backbones.r50_fpn_1x
-    backbone.cfg_url = samples_source/backbone.cfg_url
+    backbone.cfg_url = samples_source / backbone.cfg_url
 
     model = model_type.model(backbone=backbone, num_classes=81)
 
