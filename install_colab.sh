a693ac735a44f56a1bf22e0f69884fb64b2e591c
Lucas Vazquez <lucasgouvaz@gmail.com>
2021-03-22 19:25:02 -0300

    install_colab install mmdet via cloning

--- install_colab.sh
+++ install_colab.sh
@@ -2,4 +2,8 @@ pip install git+git://github.com/airctic/icevision.git#egg=icevision[all] --upgr
 pip install git+git://github.com/airctic/icedata.git --upgrade
 
 pip install mmcv-full=="1.2.5" -f https://download.openmmlab.com/mmcv/dist/cu101/torch1.7.0/index.html --upgrade
-pip install mmdet --upgrade
+#pip install mmdet --upgrade
+
+# Install mmdetection via cloning because of config folder
+git clone https://github.com/open-mmlab/mmdetection.git
+pip install -e mmdetection -q
