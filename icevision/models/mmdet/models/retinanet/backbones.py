2710a16173f14a98fd180b1449cfe3cd8b7e349b
ai-fast-track <farid@itecpedia.com>
2021-03-22 11:52:20 -0400

    renamed backbones using full name: resnet, resnext

--- icevision/models/mmdet/models/retinanet/backbones.py
+++ icevision/models/mmdet/models/retinanet/backbones.py
@@ -1,74 +1,73 @@
 __all__ = [
-    "r50_caffe_fpn_1x",
-    "r50_fpn_1x",
-    "r50_fpn_2x",
-    "r101_caffe_fpn_1x",
-    "r101_fpn_1x",
-    "r101_fpn_2x",
-    "x101_32x4d_fpn_1x",
-    "x101_32x4d_fpn_2x",
-    "x101_64x4d_fpn_1x",
-    "x101_64x4d_fpn_2x",
+    "resnet50_caffe_fpn_1x",
+    "resnet50_fpn_1x",
+    "resnet50_fpn_2x",
+    "resnet101_caffe_fpn_1x",
+    "resnet101_fpn_1x",
+    "resnet101_fpn_2x",
+    "resnext101_32x4d_fpn_1x",
+    "resnext101_32x4d_fpn_2x",
+    "resnext101_64x4d_fpn_1x",
+    "resnext101_64x4d_fpn_2x",
 ]
 
 from icevision.models.mmdet.utils import *
 
-
-r50_caffe_fpn_1x = MMDetBackboneConfig(
+resnet50_caffe_fpn_1x = MMDetBackboneConfig(
     model_name="retinanet",
     cfg_url=mmdet_base_url / "configs/retinanet/retinanet_r50_caffe_fpn_1x_coco.py",
     weights_url="http://download.openmmlab.com/mmdetection/v2.0/retinanet/retinanet_r50_caffe_fpn_1x_coco/retinanet_r50_caffe_fpn_1x_coco_20200531-f11027c5.pth",
 )
 
-r50_fpn_1x = MMDetBackboneConfig(
+resnet50_fpn_1x = MMDetBackboneConfig(
     model_name="retinanet",
-    cfg_url=mmdet_base_url / "configs/_base_/models/retinanet_r50_fpn.py",
+    cfg_url=mmdet_base_url / "configs/retinanet/retinanet_r50_fpn_1x_coco.py",
     weights_url="http://download.openmmlab.com/mmdetection/v2.0/retinanet/retinanet_r50_fpn_1x_coco/retinanet_r50_fpn_1x_coco_20200130-c2398f9e.pth",
 )
 
-r50_fpn_2x = MMDetBackboneConfig(
+resnet50_fpn_2x = MMDetBackboneConfig(
     model_name="retinanet",
     cfg_url=mmdet_base_url / "configs/retinanet/retinanet_r50_fpn_2x_coco.py",
     weights_url="http://download.openmmlab.com/mmdetection/v2.0/retinanet/retinanet_r50_fpn_1x_coco/retinanet_r50_fpn_1x_coco_20200130-c2398f9e.pth",
 )
 
-r101_caffe_fpn_1x = MMDetBackboneConfig(
+resnet101_caffe_fpn_1x = MMDetBackboneConfig(
     model_name="retinanet",
     cfg_url=mmdet_base_url / "configs/retinanet/retinanet_r101_caffe_fpn_1x_coco.py",
     weights_url="http://download.openmmlab.com/mmdetection/v2.0/retinanet/retinanet_r101_caffe_fpn_1x_coco/retinanet_r101_caffe_fpn_1x_coco_20200531-b428fa0f.pth",
 )
 
-r101_fpn_1x = MMDetBackboneConfig(
+resnet101_fpn_1x = MMDetBackboneConfig(
     model_name="retinanet",
     cfg_url=mmdet_base_url / "configs/retinanet/retinanet_r101_fpn_1x_coco.py",
     weights_url="http://download.openmmlab.com/mmdetection/v2.0/retinanet/retinanet_r101_fpn_1x_coco/retinanet_r101_fpn_1x_coco_20200130-7a93545f.pth",
 )
 
-r101_fpn_2x = MMDetBackboneConfig(
+resnet101_fpn_2x = MMDetBackboneConfig(
     model_name="retinanet",
     cfg_url=mmdet_base_url / "configs/retinanet/retinanet_r101_fpn_2x_coco.py",
     weights_url="http://download.openmmlab.com/mmdetection/v2.0/retinanet/retinanet_r101_fpn_2x_coco/retinanet_r101_fpn_2x_coco_20200131-5560aee8.pth",
 )
 
-x101_32x4d_fpn_1x = MMDetBackboneConfig(
+resnext101_32x4d_fpn_1x = MMDetBackboneConfig(
     model_name="retinanet",
     cfg_url=mmdet_base_url / "configs/retinanet/retinanet_x101_32x4d_fpn_1x_coco.py",
     weights_url="http://download.openmmlab.com/mmdetection/v2.0/retinanet/retinanet_x101_32x4d_fpn_1x_coco/retinanet_x101_32x4d_fpn_1x_coco_20200130-5c8b7ec4.pth",
 )
 
-x101_32x4d_fpn_2x = MMDetBackboneConfig(
+resnext101_32x4d_fpn_2x = MMDetBackboneConfig(
     model_name="retinanet",
     cfg_url=mmdet_base_url / "configs/retinanet/retinanet_x101_32x4d_fpn_2x_coco.py",
     weights_url="http://download.openmmlab.com/mmdetection/v2.0/retinanet/retinanet_x101_32x4d_fpn_2x_coco/retinanet_x101_32x4d_fpn_2x_coco_20200131-237fc5e1.pth",
 )
 
-x101_64x4d_fpn_1x = MMDetBackboneConfig(
+resnext101_64x4d_fpn_1x = MMDetBackboneConfig(
     model_name="retinanet",
     cfg_url=mmdet_base_url / "configs/retinanet/retinanet_x101_64x4d_fpn_1x_coco.py",
     weights_url="http://download.openmmlab.com/mmdetection/v2.0/retinanet/retinanet_x101_64x4d_fpn_1x_coco/retinanet_x101_64x4d_fpn_1x_coco_20200130-366f5af1.pth",
 )
 
-x101_64x4d_fpn_2x = MMDetBackboneConfig(
+resnext101_64x4d_fpn_2x = MMDetBackboneConfig(
     model_name="retinanet",
     cfg_url=mmdet_base_url / "configs/retinanet/retinanet_x101_64x4d_fpn_2x_coco.py",
     weights_url="http://download.openmmlab.com/mmdetection/v2.0/retinanet/retinanet_x101_64x4d_fpn_2x_coco/retinanet_x101_64x4d_fpn_2x_coco_20200131-bca068ab.pth",
