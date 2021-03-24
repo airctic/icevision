5fb61d47dcbefb76877d74dcae58ed32eb552556
FraPochetti <fra.pochetti@gmail.com>
2021-03-24 10:05:24 +0100

    fix model device

--- icevision/models/yolo/yolov5/model.py
+++ icevision/models/yolo/yolov5/model.py
@@ -24,7 +24,7 @@ def model(
 
     device = (
         torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        if device is not None
+        if device is None
         else device
     )
 
