9d64f2559246706d9ab922f4753261aad4e49c69
ai-fast-track <farid@itecpedia.com>
2021-03-24 23:54:38 -0400

    call backbone method for mask_rcnn model()

--- tests/models/torchvision_models/mask_rcnn/test_model.py
+++ tests/models/torchvision_models/mask_rcnn/test_model.py
@@ -3,15 +3,15 @@ from icevision.models.torchvision import mask_rcnn
 
 
 def test_mask_rcnn_default_param_groups():
-    model = mask_rcnn.model(num_classes=4)
+    model = models.torchvision.mask_rcnn.model(num_classes=4)
 
     param_groups = model.param_groups()
     assert len(param_groups) == 8
 
 
 def test_mask_rcnn_mobile_param_groups():
-    backbone = backbones.mobilenet(pretrained=False)
-    model = mask_rcnn.model(num_classes=6, backbone=backbone)
+    backbone = backbones.mobilenet
+    model = models.torchvision.mask_rcnn.model(num_classes=6, backbone=backbone(pretrained=False))
 
     param_groups = model.param_groups()
     assert len(param_groups) == 6
