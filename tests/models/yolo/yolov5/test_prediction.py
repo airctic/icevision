84eb3c5491f015a5064921467c9aa4b824a152f5
FraPochetti <fra.pochetti@gmail.com>
2021-03-26 21:53:28 +0100

    yolov5 tests

---
+++ tests/models/yolo/yolov5/test_prediction.py
@@ -0,0 +1,32 @@
+import pytest
+from icevision.all import *
+
+
+def _test_preds(preds):
+    assert len(preds) == 3
+    assert isinstance(preds[0].detection.bboxes[0], BBox)
+    assert len(preds[0].detection.scores) == len(preds[0].detection.labels)
+    assert len(preds[2].detection.scores) == len(preds[2].detection.labels)
+
+
+@pytest.mark.parametrize(
+    "model_name",
+    ["yolov5s", "yolov5m", "yolov5l"],
+)
+def test_yolo_predict(fridge_ds, model_name):
+    _, valid_ds = fridge_ds
+    model = yolov5.model(5, img_size=384, model_name=model_name)
+    preds = yolov5.predict(model, valid_ds, detection_threshold=0.0)
+    _test_preds(preds)
+
+
+@pytest.mark.parametrize(
+    "model_name",
+    ["yolov5s", "yolov5m", "yolov5l"],
+)
+def test_yolo_predict_dl(fridge_ds, model_name):
+    _, valid_ds = fridge_ds
+    infer_dl = yolov5.infer_dl(valid_ds, batch_size=1, shuffle=False)
+    model = yolov5.model(5, img_size=384, model_name=model_name)
+    preds = yolov5.predict_dl(model, infer_dl, detection_threshold=0.0)
+    _test_preds(preds)
