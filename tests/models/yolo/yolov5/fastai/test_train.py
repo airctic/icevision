866526101711521f0d77ea80b0d258d7a1e8559e
FraPochetti <fra.pochetti@gmail.com>
2021-03-26 21:53:28 +0100

    yolov5 tests

---
+++ tests/models/yolo/yolov5/fastai/test_train.py
@@ -0,0 +1,20 @@
+import pytest
+from icevision.all import *
+
+
+@pytest.mark.parametrize(
+    "model_name",
+    ["yolov5s", "yolov5m", "yolov5l"],
+)
+def test_fastai_yolo_train(fridge_ds, model_name):
+    train_ds, valid_ds = fridge_ds
+    train_dl = yolov5.train_dl(train_ds, batch_size=3, num_workers=0, shuffle=False)
+    valid_dl = yolov5.valid_dl(valid_ds, batch_size=3, num_workers=0, shuffle=False)
+    model = yolov5.model(5, img_size=384, model_name=model_name)
+
+    metrics = [COCOMetric(metric_type=COCOMetricType.bbox)]
+    learn = yolov5.fastai.learner(
+        dls=[train_dl, valid_dl], model=model, metrics=metrics
+    )
+
+    learn.fine_tune(1, 1e-4)
---
+++ tests/models/yolo/yolov5/test_model.py
@@ -0,0 +1,15 @@
+import pytest
+from icevision.all import *
+
+
+@pytest.mark.parametrize(
+    "model_name",
+    ["yolov5s", "yolov5m", "yolov5l"],
+)
+def test_yolo_model(model_name):
+    model = yolov5.model(num_classes=5, img_size=320, model_name=model_name)
+    weights_path = get_root_dir() / "yolo" / f"{model_name}.pt"
+
+    assert weights_path.is_file() == True
+    assert len(list(model.param_groups())) == 3
+    assert model.nc == 5
