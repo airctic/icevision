6b101044cd430f8910880be001b968ad27037799
lgvaz <lgvaz42@gmail.com>
2021-04-09 20:22:03 -0300

    matplotlib backend fixup when importing yolo

--- icevision/models/__init__.py
+++ icevision/models/__init__.py
@@ -23,4 +23,11 @@ if SoftDependencies.mmdet:
     from icevision.models import mmdet
 
 if SoftDependencies.yolov5:
+    # HACK: yolov5 changes matplotlib backend here: https://github.com/ultralytics/yolov5/blob/77415a42e5975ea356393c9f1d5cff0ae8acae2c/utils/plots.py#L26
+    import matplotlib
+
+    backend = matplotlib.get_backend()
     from icevision.models import ultralytics
+
+    matplotlib.use(backend)
+    matplotlib.rcdefaults()
