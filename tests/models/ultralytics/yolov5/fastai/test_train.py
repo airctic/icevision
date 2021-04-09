808d7cbcf6aa8951f45a502ac147f7d3c26f0048
FraPochetti <fra.pochetti@gmail.com>
2021-04-09 08:57:44 +0200

    adding yolov5 backbones

--- tests/models/ultralytics/yolov5/fastai/test_train.py
+++ tests/models/ultralytics/yolov5/fastai/test_train.py
@@ -1,12 +1,13 @@
 import pytest
 from icevision.all import *
+from icevision.models.ultralytics.yolov5.backbones import *
 
 
 @pytest.mark.parametrize(
-    "model_name",
-    ["yolov5s", "yolov5m", "yolov5l"],
+    "backbone",
+    [small, medium, large],
 )
-def test_fastai_yolo_train(fridge_ds, model_name):
+def test_fastai_yolo_train(fridge_ds, backbone):
     train_ds, valid_ds = fridge_ds
     train_dl = models.ultralytics.yolov5.train_dl(
         train_ds, batch_size=3, num_workers=0, shuffle=False
@@ -14,7 +15,9 @@ def test_fastai_yolo_train(fridge_ds, model_name):
     valid_dl = models.ultralytics.yolov5.valid_dl(
         valid_ds, batch_size=3, num_workers=0, shuffle=False
     )
-    model = models.ultralytics.yolov5.model(5, img_size=384, model_name=model_name)
+    model = models.ultralytics.yolov5.model(
+        num_classes=5, img_size=384, backbone=backbone(pretrained=True)
+    )
 
     metrics = [COCOMetric(metric_type=COCOMetricType.bbox)]
     learn = models.ultralytics.yolov5.fastai.learner(
