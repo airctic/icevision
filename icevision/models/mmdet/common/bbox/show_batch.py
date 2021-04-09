8436a5f068783a079ba7cb43f39b7cb71ccc6e0b
ai-fast-track <farid@itecpedia.com>
2021-04-09 11:05:29 -0400

    refactor

--- icevision/models/mmdet/common/bbox/show_batch.py
+++ icevision/models/mmdet/common/bbox/show_batch.py
@@ -10,10 +10,9 @@ def show_batch(batch_and_records, ncols: int = 1, figsize=None, **show_samples_k
         show_samples_kwargs: Check the parameters from `show_samples`
     """
     batch, records = batch_and_records
-    tensor_images = [img for img in batch['img'][0][:]]
 
-    for tensor_image, record in zip(tensor_images, records):
-        image = tensor_image.cpu().numpy().transpose(1, 2, 0)
+    for tensor_image, record in zip(batch['img'][:], records):
+        image = tensor_image.detach().cpu().numpy().transpose(1, 2, 0)
         record.set_img(image)
 
     return show_samples(records, ncols=ncols, figsize=figsize, **show_samples_kwargs)
