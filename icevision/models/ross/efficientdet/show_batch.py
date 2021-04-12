a8ca3c53175f320edc4a5d85f8f9149825eba0f7
Lucas Vazquez <lucasgouvaz@gmail.com>
2021-04-12 15:08:50 -0300

    implements keep_images for efficientdet (#764)
    * implements keep_images for efficientdet
    
    * fixes tests

---
+++ icevision/models/ross/efficientdet/show_batch.py
@@ -0,0 +1,19 @@
+__all__ = ["show_batch"]
+
+from icevision.utils import *
+from icevision.visualize import *
+
+
+def show_batch(batch, ncols: int = 1, figsize=None, **show_samples_kwargs):
+    """Show a single batch from a dataloader.
+
+    # Arguments
+        show_samples_kwargs: Check the parameters from `show_samples`
+    """
+    (tensor_images, *_), records = batch
+
+    for tensor_image, record in zip(tensor_images, records):
+        image = tensor_to_image(tensor_image)
+        record.set_img(image)
+
+    return show_samples(records, ncols=ncols, figsize=figsize, **show_samples_kwargs)
