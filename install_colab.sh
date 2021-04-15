7c00975e7683493e351bd451563d2d41dcad7d9a
Lucas Vazquez <lucasgouvaz@gmail.com>
2021-04-15 09:07:10 -0300

    adds yolo installation to install_colab.sh

--- install_colab.sh
+++ install_colab.sh
@@ -3,3 +3,5 @@ pip install git+git://github.com/airctic/icedata.git --upgrade
 
 pip install mmcv-full=="1.2.5" -f https://download.openmmlab.com/mmcv/dist/cu101/torch1.7.0/index.html --upgrade
 pip install mmdet --upgrade
+
+pip install yolov5-icevision --upgrade
