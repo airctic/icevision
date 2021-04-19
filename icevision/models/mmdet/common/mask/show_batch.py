f2257cdde481325a97e3f5be3d43ac587024bb45
Lucas Vazquez <lucasgouvaz@gmail.com>
2021-04-19 11:30:28 -0300

    fixes mmdet tensor_to_image (#804)

--- icevision/models/mmdet/common/mask/show_batch.py
+++ icevision/models/mmdet/common/mask/show_batch.py
@@ -2,6 +2,7 @@ __all__ = ["show_batch"]
 
 from icevision.utils import *
 from icevision.visualize import *
+from icevision.models.mmdet.common.utils import *
 
 
 def show_batch(batch_and_records, ncols: int = 1, figsize=None, **show_samples_kwargs):
@@ -12,7 +13,7 @@ def show_batch(batch_and_records, ncols: int = 1, figsize=None, **show_samples_k
     batch, records = batch_and_records
 
     for tensor_image, record in zip(batch["img"][:], records):
-        image = tensor_image.detach().cpu().numpy().transpose(1, 2, 0)
+        image = mmdet_tensor_to_image(tensor_image)
         record.set_img(image)
 
     return show_samples(records, ncols=ncols, figsize=figsize, **show_samples_kwargs)
