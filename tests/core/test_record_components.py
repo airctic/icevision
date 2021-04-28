09b407a910893957b6e971530b12fffdc76e5fc2
Rahul Somani <rsomani95@gmail.com>
2021-04-29 00:20:41 +0530

    add test for multi-label case

--- tests/core/test_record_components.py
+++ tests/core/test_record_components.py
@@ -43,3 +43,25 @@ def test_classification_single_label(dummy_class_map, label_ids):
         # Only one label must be assigned
         assert all(rec.classification._autofix().values())
         assert rec.classification.one_hot_encoded().sum() == 1
+
+
+@pytest.mark.parametrize(
+    "label_ids",
+    [
+        ([0, 1, 2]),
+        ([0, 1]),
+        ([0]),
+    ],
+)
+def test_one_hot_encodings(dummy_class_map_elaborate, label_ids):
+    rec = BaseRecord([ClassificationLabelsRecordComponent(is_multilabel=True)])
+    rec.classification.set_class_map(dummy_class_map_elaborate)
+    rec.classification.set_labels_by_id(label_ids)
+
+    assert all(rec.classification._autofix().values())
+
+    # Ensure we have the correct no. of labels and that they are indeed
+    # one-hot encoded
+    one_hot_values = rec.classification.one_hot_encoded()
+    assert one_hot_values.sum() == len(label_ids)
+    assert np.unique(one_hot_values).tolist() == [0, 1]
