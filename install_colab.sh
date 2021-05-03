13cd65c0dcddcc022df228161ca65d200e20e623
Lucas Vazquez <lucasgouvaz@gmail.com>
2021-05-03 11:04:38 -0300

    pins mmdet on colab install script

--- install_colab.sh
+++ install_colab.sh
@@ -2,6 +2,6 @@ pip install git+git://github.com/airctic/icevision.git\#egg=icevision[all] --upg
 pip install git+git://github.com/airctic/icedata.git --upgrade
 
 pip install mmcv-full=="1.3.1" -f https://download.openmmlab.com/mmcv/dist/cu101/torch1.8.0/index.html --upgrade
-pip install mmdet --upgrade
+pip install mmdet=="2.11.0" --upgrade
 
 pip install yolov5-icevision --upgrade
