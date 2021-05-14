34a4e9a5882b69f46a551f04cdd5ee125b4d27d6
ai-fast-track <farid@itecpedia.com>
2021-05-14 14:55:06 -0400

    rename file

--- icevision/models/mmdet/models/retinanet/backbones/timm/timm_mobilenet_fpn.py
+++ icevision/models/mmdet/models/retinanet/backbones/timm/timm_mobilenet_fpn.py
@@ -16,21 +16,7 @@ __all__ = [
     "tf_mobilenetv3_small_minimal_100",
 ]
 
-import icevision
-from icevision.imports import *
-from icevision.models.mmdet.utils import *
-
-base_config_path = Path(icevision.__file__).parent / "models/mmdet/models/retinanet"
-
-
-class MMDetTimmRetinanetBackboneConfig(MMDetTimmBackboneConfig):
-    def __init__(self, backbone_dict, **kwargs):
-        super().__init__(
-            model_name="retinanet",
-            config_path=base_config_path / "retinanet_no_backbone_fpn.py",
-            backbone_dict=backbone_dict,
-            **kwargs
-        )
+from icevision.models.mmdet.models.retinanet.backbones.timm.common import *
 
 
 mobilenetv2_100 = MMDetTimmRetinanetBackboneConfig(
