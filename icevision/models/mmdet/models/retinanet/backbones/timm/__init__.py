925f916457283b83b0c26a19861830c037f27773
ai-fast-track <farid@itecpedia.com>
2021-05-14 21:16:14 -0400

    expose separate model families

--- icevision/models/mmdet/models/retinanet/backbones/timm/__init__.py
+++ icevision/models/mmdet/models/retinanet/backbones/timm/__init__.py
@@ -1,3 +1,6 @@
 from icevision.models.mmdet.models.retinanet.backbones.timm.common import *
-from icevision.models.mmdet.models.retinanet.backbones.timm.mobilenet import *
-from icevision.models.mmdet.models.retinanet.backbones.timm.resnest import *
+# from icevision.models.mmdet.models.retinanet.backbones.timm.mobilenet import *
+# from icevision.models.mmdet.models.retinanet.backbones.timm.resnest import *
+
+from icevision.models.mmdet.models.retinanet.backbones.timm import mobilenet
+from icevision.models.mmdet.models.retinanet.backbones.timm import resnest
