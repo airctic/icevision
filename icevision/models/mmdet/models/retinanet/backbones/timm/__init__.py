28a1ed70d8b34e6aa1e686bf23031b9d83d0a962
ai-fast-track <farid@itecpedia.com>
2021-05-17 13:03:21 -0400

    applied black

--- icevision/models/mmdet/models/retinanet/backbones/timm/__init__.py
+++ icevision/models/mmdet/models/retinanet/backbones/timm/__init__.py
@@ -1,4 +1,5 @@
 from icevision.models.mmdet.models.retinanet.backbones.timm.common import *
+
 # from icevision.models.mmdet.models.retinanet.backbones.timm.mobilenet import *
 # from icevision.models.mmdet.models.retinanet.backbones.timm.resnest import *
 
