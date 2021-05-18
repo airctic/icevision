a895502359d1c6d22aabfe531b123a2be6c5dcc7
ai-fast-track <farid@itecpedia.com>
2021-05-18 15:41:10 -0400

    remove commented code

--- icevision/models/mmdet/models/retinanet/backbones/__init__.py
+++ icevision/models/mmdet/models/retinanet/backbones/__init__.py
@@ -1,5 +1,2 @@
-# from icevision.models.mmdet.models.retinanet.backbones.timm.timm_mobilenet_fpn import *
-# from icevision.models.mmdet.models.retinanet.backbones.timm.timm_resnest_fpn import *
-
 from icevision.models.mmdet.models.retinanet.backbones import timm
 from icevision.models.mmdet.models.retinanet.backbones import mmdet
--- icevision/models/mmdet/models/retinanet/backbones/timm/__init__.py
+++ icevision/models/mmdet/models/retinanet/backbones/timm/__init__.py
@@ -1,7 +1,4 @@
 from icevision.models.mmdet.models.retinanet.backbones.timm.common import *
 
-# from icevision.models.mmdet.models.retinanet.backbones.timm.mobilenet import *
-# from icevision.models.mmdet.models.retinanet.backbones.timm.resnest import *
-
 from icevision.models.mmdet.models.retinanet.backbones.timm import mobilenet
 from icevision.models.mmdet.models.retinanet.backbones.timm import resnest
