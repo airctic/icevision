6c458eff4df6847a095d6b5c36c93a4f576c6e84
ai-fast-track <farid@itecpedia.com>
2021-05-27 16:33:09 -0400

    create timm model from model_name

--- icevision/models/mmdet/backbones/timm/common.py
+++ icevision/models/mmdet/backbones/timm/common.py
@@ -1,16 +1,31 @@
 __all__ = ["MMDetTimmBase"]
 
 import torch.nn as nn
+from timm.models.registry import *
 
 
 class MMDetTimmBase(nn.Module):
-    def __init__(self, pretrained=True, out_indices=(0, 1, 2, 3, 4), **kwargs):
+    def __init__(
+        self,
+        model_name: str = None,
+        pretrained: bool = True,  # doesn't matter
+        out_indices: Collection[int] = (1, 2, 3, 4),
+        norm_eval: bool = True,
+    ):
+
         super().__init__()
+        print(model_name)
+        self.model_name = model_name
         self.pretrained = pretrained
         self.out_indices = out_indices
+        self.norm_eval = norm_eval
+        model_fn = model_entrypoint(self.model_name)
+        self.model = model_fn(
+            pretrained=self.pretrained, features_only=True, out_indices=out_indices
+        )
 
     def init_weights(self, pretrained=None):
         pass
 
-    def forward(self, x):  # should return a tuple
-        return self.model(x)
+    def forward(self, x) -> Tuple[Tensor]:  # should return a tuple
+        return tuple(self.model(x))
