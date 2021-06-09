88fa3ec6ff895564cd6400424110908568016be9
ai-fast-track <farid@itecpedia.com>
2021-06-09 09:13:12 -0400

    pass config_path argument to parent class

--- icevision/models/mmdet/models/retinanet/backbones/timm/common.py
+++ icevision/models/mmdet/models/retinanet/backbones/timm/common.py
@@ -6,14 +6,12 @@ import icevision
 from icevision.imports import *
 from icevision.models.mmdet.utils import *
 
-base_config_path = Path(icevision.__file__).parent / "models/mmdet/models/retinanet"
-
 
 class MMDetTimmRetinanetBackboneConfig(MMDetTimmBackboneConfig):
     def __init__(self, config_path, backbone_dict, weights_url=None, **kwargs):
         super().__init__(
             model_name="retinanet",
-            config_path=base_config_path,
+            config_path=config_path,
             backbone_dict=backbone_dict,
             weights_url=weights_url,
             **kwargs
