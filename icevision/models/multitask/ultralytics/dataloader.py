bcc02569df07be96d6f57c7a7d7aa789fe1330be
Rahul Somani <rsomani95@gmail.com>
2021-06-13 09:57:21 +0530

    basic dataloader

---
+++ icevision/models/multitask/ultralytics/dataloader.py
@@ -0,0 +1,65 @@
+from icevision.imports import *
+from icevision.core import *
+from icevision.models.utils import *
+from icevision.models.ultralytics.yolov5.dataloaders import (
+    _build_train_sample as _build_train_detection_sample,
+)
+from torch import Tensor
+from icevision.models.multitask.data.dtypes import *
+
+
+def build_single_aug_batch(
+    records: Sequence[RecordType],
+) -> Tuple[TensorList, TensorDict, Sequence[RecordType]]:
+    """Builds a batch in the format required by the model when training.
+
+    # Arguments
+        records: A `Sequence` of records.
+
+    # Returns
+        A tuple with two items. The first will be a tuple like `(images, detection_targets, classification_targets)`
+        in the input format required by the model. The second will be an updated list
+        of the input records.
+
+    # Examples
+
+    Use the result of this function to feed the model.
+    ```python
+    batch, records = build_train_batch(records)
+    outs = model(*batch)
+    ```
+    """
+    images, targets = [], []
+    classification_targets = defaultdict(list)
+
+    for i, record in enumerate(records):
+        image, target = _build_train_detection_sample(record)
+        images.append(image)
+
+        if target.numel() > 0:
+            target[:, 0] = i
+
+        targets.append(target)
+
+        # Classification
+        for comp in record.components:
+            name = comp.task.name
+            if isinstance(comp, ClassificationLabelsRecordComponent):
+                if comp.is_multilabel:
+                    labels = comp.one_hot_encoded()
+                    classification_targets[name].append(labels)
+                else:
+                    labels = comp.label_ids
+                    classification_targets[name].extend(labels)
+
+    classification_targets = {k: tensor(v) for k, v in classification_targets.items()}
+
+    return (
+        torch.stack(images, 0),
+        torch.cat(targets, 0),
+        classification_targets,
+    ), records
+
+
+def build_multi_aug_batch():
+    raise NotImplementedError
