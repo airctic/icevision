d20ed4537c77eb3ff6ea988566c5d3de62ac7b8f
Rahul Somani <rsomani95@gmail.com>
2021-06-15 09:01:55 +0530

    doc, type anno

--- icevision/models/multitask/data/dataloading_utils.py
+++ icevision/models/multitask/data/dataloading_utils.py
@@ -6,13 +6,27 @@ as it only slightly modifies an existing function.
 __all__ = ["unload_records"]
 
 
-from typing import Dict, Optional, Callable
+from icevision.core.record_type import RecordType
+from typing import Any, Dict, Optional, Callable, Sequence, Tuple
 
 
-def unload_records(build_batch: Callable, build_batch_kwargs: Optional[Dict] = None):
+def unload_records(
+    build_batch: Callable, build_batch_kwargs: Optional[Dict] = None
+) -> Tuple[Tuple[Any, ...], Sequence[RecordType]]:
     """
-    This decorator function unloads records to not carry them around after batch creation
-      and will also accept any additional args required by the `build_batch`` function
+    This decorator function unloads records to not carry them around after batch creation.
+      It also optionally accepts `build_batch_kwargs` that are to be passed into
+      `build_batch`. These aren't accepted as keyword arguments as those are reserved
+      for PyTorch's DataLoader class which is used later in this chain of function calls
+
+    Args:
+        build_batch (Callable): A collate function that describes how to mash records
+                                into a batch of inputs for a model
+        build_batch_kwargs (Optional[Dict], optional): Keyword arguments to pass into
+                                                       `build_batch`. Defaults to None.
+
+    Returns:
+        Tuple[Tuple[Any, ...], Sequence[RecordType]]: [description]
     """
     build_batch_kwargs = build_batch_kwargs or {}
     assert isinstance(build_batch_kwargs, dict)
