8eb9cff71a2c507464cd1e1e8d25af6a0522f4ab
Rahul Somani <rsomani95@gmail.com>
2021-06-16 11:24:01 +0530

    add tensortuple dtype

--- icevision/models/multitask/utils/dtypes.py
+++ icevision/models/multitask/utils/dtypes.py
@@ -3,11 +3,19 @@ from torch import Tensor
 import numpy as np
 import torch
 
-__all__ = ["ImgMetadataDict", "TensorList", "TensorDict", "ArrayList", "ArrayDict"]
+__all__ = [
+    "ImgMetadataDict",
+    "TensorList",
+    "TensorTuple",
+    "TensorDict",
+    "ArrayList",
+    "ArrayDict",
+]
 
 ImgMetadataDict = Dict[str, Union[Tuple[int], np.ndarray]]
 TensorList = List[Tensor]
 TensorDict = Dict[str, Tensor]
+TensorTuple = Tuple[Tensor]
 ArrayList = List[np.ndarray]
 ArrayDict = Dict[str, np.ndarray]
 
