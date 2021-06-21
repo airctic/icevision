57f6ff8e317b5b87eb8bf9c82722e8753662254b
ai-fast-track <farid@itecpedia.com>
2021-06-21 19:45:30 -0400

    use different mmcv, mmdet versions for cuda 11

--- icevision_install.sh
+++ icevision_install.sh
@@ -5,9 +5,6 @@ case ${cuda_version_major} in
       echo "- Installing torch and its dependencies"
       pip install torch==1.8.1+cu102 torchvision==0.9.1+cu102 torchaudio===0.8.0 -f https://download.pytorch.org/whl/torch_stable.html --upgrade -q
       pip install torchtext==0.9.0 --upgrade -q
-      pip install mmcv-full==1.3.7 -f https://download.openmmlab.com/mmcv/dist/cu102/torch1.8.0/index.html --upgrade -q
-      echo "- Installing mmdet"
-      pip install mmdet==2.13.0 --upgrade -q
       echo "- Installing fastai"
       pip install fastai==2.3.1 --upgrade -q
       echo "- Installing icevision from master"
@@ -16,17 +13,17 @@ case ${cuda_version_major} in
       pip install git+git://github.com/airctic/icedata.git --upgrade -q
       echo "- Installing yolov5-icevision" 
       pip install yolov5-icevision --upgrade -q 
+      echo "- Installing mmcv"
+      pip install mmcv-full==1.3.7 -f https://download.openmmlab.com/mmcv/dist/cu102/torch1.8.0/index.html --upgrade -q
+      echo "- Installing mmdet"
+      pip install mmdet==2.13.0 --upgrade -q
       echo "icevision installation finished!"   
       ;; 
    11)  
       echo "Installing icevision + dependencices for cuda ${1}"
       echo "- Installing torch and its dependencies"
-      pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111torchaudio===0.8.0 -f https://download.pytorch.org/whl/torch_stable.html --upgrade -q
+      pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio===0.8.0 -f https://download.pytorch.org/whl/torch_stable.html --upgrade -q
       pip install torchtext==0.9.0 --upgrade -q
-      echo "- Installing mmcv"
-      pip install mmcv-full==1.3.7 -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.8.0/index.html --upgrade -q
-      echo "- Installing mmdet"
-      pip install mmdet==2.13.0 --upgrade -q
       echo "- Installing fastai"
       pip install fastai==2.3.1 --upgrade -q
       echo "- Installing icevision from master"
@@ -35,6 +32,10 @@ case ${cuda_version_major} in
       pip install git+git://github.com/airctic/icedata.git --upgrade -q
       echo "- Installing yolov5-icevision" 
       pip install yolov5-icevision --upgrade -q 
+      echo "- Installing mmcv"
+      pip install mmcv-full==1.3.3 -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.8.0/index.html --upgrade -q
+      echo "- Installing mmdet"
+      pip install mmdet==2.12.0 --upgrade -q
       echo "icevision installation finished!" 
     ;;       
    *)  
