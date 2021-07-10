2dc9e7268de4980011c868edcd725753897495e7
AI Fast Track <59614262+ai-fast-track@users.noreply.github.com>
2021-07-09 21:35:30 -0400

    Fix icevision installation in Colab (#887)
    * google-colab package needs requests==2.23
    
    * fix error in colab installation by selecting versions
    
    * remove ! and use -U everywhere
    
    * fixed version numbers
    
    * restart runtime after installation

--- install_colab.sh
+++ install_colab.sh
@@ -1,20 +1,33 @@
 echo "Installing icevision + dependencices for CUDA 10"
+echo "Uninstalling some dependencies to prevent errors"
+pip uninstall torchvision -y
+pip uninstall fastai -y
+
+ echo "Installing some dependencies to prevent errors"
+pip install PyYAML>=5.1 -U -q
+pip install datascience -U -q
+pip install tensorflow==2.4.0 -U -q
+pip install google-colab -U -q
+
 echo "- Installing torch and its dependencies"
-pip install torchtext==0.9.0 --upgrade -q
-pip install torch==1.8.1+cu102 torchvision==0.9.1+cu102 torchaudio===0.8.0 -f https://download.pytorch.org/whl/torch_stable.html --upgrade -q
+echo "- Installing torch and its dependencies"
+pip install torchtext==0.9.0 -U -q
+pip install torch==1.8.0+cu101 torchvision==0.9.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html -U -q
+
 echo "- Installing fastai"
-pip install fastai==2.3.1 --upgrade -q
+pip install fastai==2.3.1 -U -q
+
 echo "- Installing icevision from master"
-pip install git+git://github.com/airctic/icevision.git\#egg=icevision[all] --upgrade -q
+pip install git+git://github.com/airctic/icevision.git@fix-colab-install -U -q
 echo "- Installing icedata from master"      
-pip install git+git://github.com/airctic/icedata.git --upgrade -q
+pip install git+git://github.com/airctic/icedata.git -U -q
 echo "- Installing yolov5-icevision" 
-pip install yolov5-icevision --upgrade -q 
+pip install yolov5-icevision -U -q 
 echo "- Installing mmcv"
-pip install mmcv-full==1.3.7 -f https://download.openmmlab.com/mmcv/dist/cu102/torch1.8.0/index.html --upgrade -q
+pip install mmcv-full==1.3.7 -f https://download.openmmlab.com/mmcv/dist/cu101/torch1.8.0/index.html -U -q
 echo "- Installing mmdet"
-pip install mmdet==2.13.0 --upgrade -q
-echo "icevision installation finished!"  
+pip install mmdet==2.13.0 -U -q
+echo "icevision installation finished"  
 
 # restart notebook
 echo "Restarting runtime!"
