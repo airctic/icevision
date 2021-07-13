0088ad2f1b933a054edee844424f2b48b879579f
AI Fast Track <59614262+ai-fast-track@users.noreply.github.com>
2021-07-13 12:27:06 -0400

    Added script for icevision inference installation (#893)
    * added script for icevision inference installation
    
    * install torchvision and torchtext first

---
+++ install_icevision_inference.sh
@@ -0,0 +1,34 @@
+platform="${1}" 
+case ${platform} in 
+   colab)
+      echo "- Installing icevision inference in Colab"  
+      echo "Uninstalling some dependencies to prevent errors"
+      pip uninstall fastai -y
+      pip uninstall torchvision -y
+      pip uninstall torchtext -y
+
+      echo "- Updating torchvision"
+      pip install torchvision==0.9.0 -U -q
+      
+      echo "- Updating torchtext"
+      pip install torchtext==0.9.0 -U -q 
+
+      echo "Installing some dependencies to prevent errors"
+      pip install PyYAML>=5.1 -U -q
+      pip install datascience -U -q
+      pip install tensorflow==2.4.0 -U -q
+      pip install google-colab -U -q
+      
+      
+      echo "- Installing icevision and icedata"
+      pip install git+git://github.com/airctic/icevision.git#egg=icevision[inference] -U -q
+      pip install icedata -U -q
+      echo "icevision installation finished!"   
+      ;; 
+   *)  # Installing only icevision and icedata
+      echo "- Installing icevision inference"
+      pip install git+git://github.com/airctic/icevision.git#egg=icevision[inference] -U -q
+      pip install icedata -U -q
+      echo "icevision installation finished!"
+      ;; 
+esac 
