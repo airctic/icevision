e33f9a1d44772d6fd8a6089dee98fa1af97e6367
Rahul Somani <rsomani95@gmail.com>
2021-07-30 10:28:19 +0530

    vastly simplified pipeline

--- icevision/models/multitask/data/dataset.py
+++ icevision/models/multitask/data/dataset.py
@@ -130,16 +130,16 @@ class HybridAugmentationsRecordDataset(Dataset):
         records: List[dict],
         classification_transforms_groups: dict,
         detection_transforms: Optional[tfms.Transform] = None,
-        norm_mean: Collection[float] = [0.485, 0.456, 0.406],
-        norm_std: Collection[float] = [0.229, 0.224, 0.225],
+        # norm_mean: Collection[float] = [0.485, 0.456, 0.406],
+        # norm_std: Collection[float] = [0.229, 0.224, 0.225],
         debug: bool = False,
     ):
         "Return `PIL.Image` when `debug=True`"
         self.records = records
         self.classification_transforms_groups = classification_transforms_groups
         self.detection_transforms = detection_transforms
-        self.norm_mean = norm_mean
-        self.norm_std = norm_std
+        # self.norm_mean = norm_mean
+        # self.norm_std = norm_std
         self.debug = debug
         self.validate()
 
@@ -183,50 +183,36 @@ class HybridAugmentationsRecordDataset(Dataset):
         record = self.load_record(i)
 
         # Keep a copy of the orig img as it gets modified by albu
-        original_img = deepcopy(record.img)
-        if isinstance(original_img, np.ndarray):
-            original_img = PIL.Image.fromarray(original_img)
+        original_img: PIL.Image.Image = deepcopy(record.img)
 
         # Do detection transform and assign it to the detection task
         if self.detection_transforms is not None:
             record = self.detection_transforms(record)
-
-        record.add_component(ImageRecordComponent(Task("detection")))
-        record.detection.set_img(record.img)
+            record.add_component(ImageRecordComponent(Task("detection")))
+            record.detection.set_img(record.img)
 
         if self.debug:
             print(f"Fetching Item #{i}")
 
         # Do classification transforms
-        for group in self.classification_transforms_groups.values():
-            img_tfms = group["transforms"]
-            tfmd_img = img_tfms(original_img)
-            if self.debug:
-                print(f"  Group: {group['tasks']}, ID: {id(tfmd_img)}")
-
-            # NOTE:
-            # Setting the same img twice (to diff parts in memory) but it's ok cuz we will unload the record later
-            for task in group["tasks"]:
-                # record.add_component(ImageRecordComponent(Task(task))) # TODO FIXME: This throws a weird error idk why
-                comp = getattr(record, task)
-                comp.add_component(ImageRecordComponent())
-                comp.set_img(tfmd_img)
+        if self.classification_transforms_groups is not None:
+            for group in self.classification_transforms_groups.values():
+                img_tfms = group["transforms"]
+                tfmd_img = img_tfms(original_img)
                 if self.debug:
-                    print(f"   - Task: {task}, ID: {id(tfmd_img)}")
+                    print(f"  Group: {group['tasks']}, ID: {id(tfmd_img)}")
 
-        # This is a bit verbose, but allows us to return PIL images for easy debugging.
-        # Else, it returns normalized numpy arrays, like usual icevision datasets
-        for comp in record.components:
-            if isinstance(comp, ImageRecordComponent):
-                # Convert to `np.ndarray` if it isn't already
-                if isinstance(comp.img, PIL.Image.Image):
-                    comp.set_img(np.array(comp.img))
-                if self.debug:  # for debugging only
-                    comp.set_img(PIL.Image.fromarray(comp.img))
-                else:
-                    comp.set_img(
-                        normalize(comp.img, mean=self.norm_mean, std=self.norm_std)
-                    )
+                # NOTE:
+                # Setting the same img twice (to diff parts in memory) but it's ok cuz we will unload the record later
+                for task in group["tasks"]:
+                    # TODO FIXME: This adds a component but doesn't display it when printing
+                    # Also, doing `set_img` overrides the base  `record.img`
+                    # record.add_component(ImageRecordComponent(Task(task)))
+                    comp = getattr(record, task)
+                    comp.add_component(ImageRecordComponent())
+                    comp.set_img(tfmd_img)
+                    if self.debug:
+                        print(f"   - Task: {task}, ID: {id(tfmd_img)}")
 
         return record
 
