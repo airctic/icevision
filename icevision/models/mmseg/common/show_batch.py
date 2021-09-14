202d3b49a96f8858489ae7963f588d9f0144aaf0
Nicolas Jaccard <nicolas.jaccard@orbis.org>
2021-09-14 15:48:58 +0000

    Attempt to fix show_batch for mmseg

--- icevision/models/mmseg/common/show_batch.py
+++ icevision/models/mmseg/common/show_batch.py
@@ -3,6 +3,7 @@ __all__ = ["show_batch"]
 from icevision.utils import *
 from icevision.visualize import *
 from icevision.models.mmseg.common.utils import *
+from icevision.core import *
 
 
 def show_batch(batch_and_records, ncols: int = 1, figsize=None, **show_samples_kwargs):
@@ -12,8 +13,13 @@ def show_batch(batch_and_records, ncols: int = 1, figsize=None, **show_samples_k
     """
     batch, records = batch_and_records
 
-    for tensor_image, record in zip(batch["img"][:], records):
-        image = mmdet_tensor_to_image(tensor_image)
+    for tensor_image, gt_masks, record in zip(
+        batch["img"][:], batch["gt_semantic_seg"], records
+    ):
+        image = mmseg_tensor_to_image(tensor_image)
         record.set_img(image)
+
+        mask = MaskArray(gt_masks.cpu().numpy().squeeze())
+        record.segmentation.set_mask_array(mask)
 
     return show_samples(records, ncols=ncols, figsize=figsize, **show_samples_kwargs)
