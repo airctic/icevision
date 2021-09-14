ac25ae765c6ea6a355b7aaceb0127903025c7e48
Nicolas Jaccard <nicolas.jaccard@orbis.org>
2021-09-14 13:07:55 +0000

    Getting closer to final structure

--- icevision/models/mmseg/common/__init__.py
+++ icevision/models/mmseg/common/__init__.py
@@ -1,7 +1,12 @@
 from icevision.models.mmseg.common.loss import *
-from icevision.models.mmseg.common import mask
 
 from icevision.models.mmseg.common.dataloaders import *
 from icevision.models.mmseg.common.prediction import *
 from icevision.models.mmseg.common.show_results import *
 from icevision.models.mmseg.common.show_batch import *
+
+# Soft dependencies
+from icevision.soft_dependencies import SoftDependencies
+
+if SoftDependencies.fastai:
+    from icevision.models.mmseg.common import fastai
--- icevision/models/mmseg/common/segmentors/encoder_decoder/__init__.py
+++ icevision/models/mmseg/common/segmentors/encoder_decoder/__init__.py
@@ -1,2 +1,2 @@
-from icevision.models.mmseg.common.mask import *
+from icevision.models.mmseg.common import *
 from icevision.models.mmseg.common.segmentors.encoder_decoder.model import *
