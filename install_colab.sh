820d30803f691b97f485dfcf705e67b22710a794
ai-fast-track <farid@itecpedia.com>
2021-09-29 22:18:00 -0400

    remove ! from script

--- install_colab.sh
+++ install_colab.sh
@@ -1,18 +1,18 @@
-!pip install openmim -q
-!echo "- Installing mmcv"
-!mim install mmcv-full
-!echo "- Installing mmdet"
-!mim install mmdet
+pip install openmim -q
+echo "- Installing mmcv"
+mim install mmcv-full
+echo "- Installing mmdet"
+mim install mmdet
 
 
-!echo "- Installing icevision from master"
-!pip install git+git://github.com/airctic/icevision.git#egg=icevision[all] -U -q
-!echo "- Installing icedata from master"      
-!pip install git+git://github.com/airctic/icedata.git -U -q
-!echo "- Installing yolov5-icevision" 
-!pip install yolov5-icevision -U -q 
+echo "- Installing icevision from master"
+pip install git+git://github.com/airctic/icevision.git#egg=icevision[all] -U -q
+echo "- Installing icedata from master"      
+pip install git+git://github.com/airctic/icedata.git -U -q
+echo "- Installing yolov5-icevision" 
+pip install yolov5-icevision -U -q 
 
 
 # restart notebook
-!echo "Restarting runtime!"
+echo "Restarting runtime"
 exit()
