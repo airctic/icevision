b274ed69618b401ab1de7016ab5bb00caf35f560
Nicolas Jaccard <nicolas.jaccard@orbis.org>
2021-10-04 20:41:51 +0000

    Updated tests

--- tests/models/mmseg/test_show_results.py
+++ tests/models/mmseg/test_show_results.py
@@ -1,33 +1,18 @@
 import pytest
 from icevision.all import *
-from icevision.models.fastai.unet.backbones import *
+from icevision.models.mmseg.models.deeplabv3.backbones import *
+from icevision.models.mmseg.models import *
 from icevision.models.interpretation import get_samples_losses
 
 
-# @pytest.mark.parametrize(
-#     "backbone",
-#     [resnet18, resnet50, resnet101],
-# )
-# def test_show_results(camvid_ds, backbone, monkeypatch):
-#     monkeypatch.setattr(plt, "show", lambda: None)
-#     _, valid_ds = camvid_ds
-#     model = models.fastai.unet.model(
-#         num_classes=32, img_size=64, backbone=backbone(pretrained=True)
-#     )
-#     models.fastai.unet.show_results(model, valid_ds)
-
-
-# @pytest.mark.parametrize(
-#     "backbone",
-#     [resnet18, resnet50, resnet101],
-# )
-def test_plot_losses(camvid_ds, monkeypatch):
+@pytest.mark.parametrize(
+    "backbone",
+    [resnet18_d8, resnet50_d8, resnet101_d8],
+)
+def test_show_results(camvid_ds, backbone, monkeypatch):
     monkeypatch.setattr(plt, "show", lambda: None)
-    _, ds = camvid_ds
-
-    backbone = models.mmseg.deeplabv3.backbones.resnet50_d8
-
-    model = models.mmseg.deeplabv3.model(
+    _, valid_ds = camvid_ds
+    model = deeplabv3.model(
         backbone=backbone(
             pretrained=False,
         ),
@@ -35,9 +20,34 @@ def test_plot_losses(camvid_ds, monkeypatch):
     )
 
     model.eval()  # Needed to allow batch size of 1
-    model.cuda()  # Needed when ran on machine with a GPU
 
-    samples_plus_losses, preds, _ = models.mmseg.deeplabv3.interp.plot_top_losses(
+    if torch.cuda.is_available():
+        model.cuda()  # Needed when ran on machine with a GPU as data will be loaded on the device by default
+
+    deeplabv3.show_results(model, valid_ds)
+
+
+@pytest.mark.parametrize(
+    "backbone",
+    [resnet18_d8, resnet50_d8, resnet101_d8],
+)
+def test_plot_losses(camvid_ds, backbone, monkeypatch):
+    monkeypatch.setattr(plt, "show", lambda: None)
+    _, ds = camvid_ds
+
+    model = deeplabv3.model(
+        backbone=backbone(
+            pretrained=False,
+        ),
+        num_classes=32,
+    )
+
+    model.eval()  # Needed to allow batch size of 1
+
+    if torch.cuda.is_available():
+        model.cuda()  # Needed when ran on machine with a GPU as data will be loaded on the device by default
+
+    samples_plus_losses, preds, _ = deeplabv3.interp.plot_top_losses(
         model=model, dataset=ds, sort_by="loss_total", n_samples=2
     )
     assert len(samples_plus_losses) == len(ds) == len(preds)
