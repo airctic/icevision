e97574af15877f8853ee00d030216c60e58ad97b
Nicolas Jaccard <nicolas.jaccard@orbis.org>
2021-10-07 16:15:25 +0000

    Reverted workflow files to match current master

--- install_colab.sh
+++ install_colab.sh
@@ -1,10 +1,11 @@
 pip install openmim -q
 echo "- Installing mmcv"
-mim install mmcv-full==1.3.13
+mim install mmcv-full
 echo "- Installing mmdet"
-mim install mmdet==2.12.0
-echo "- Installing mmseg"
+mim install mmdet
+echo "- Installing mmdet"
 mim install mmsegmentation==0.17.0
+
 
 echo "- Installing icevision from master"
 pip install git+git://github.com/airctic/icevision.git#egg=icevision[all] -U -q
