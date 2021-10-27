2f0c74c54ed1592293916c9095e820f5fa405ebd
ai-fast-track <farid@itecpedia.com>
2021-10-27 11:21:41 -0400

    added an installation script cuda and cpu

---
+++ icevision_install.sh
@@ -0,0 +1,51 @@
+# target can be: cuda11, cuda10, cpu
+
+target="${1}" 
+case ${target} in 
+   cuda10)  
+      echo "Installing icevision + dependencices for ${1}"
+      echo "- Installing torch and its dependencies"
+      pip install torch==1.9.0+cu102 torchvision==0.10.0+cu102 -f https://download.pytorch.org/whl/torch_stable.html --upgrade -q
+
+      echo "- Installing mmcv"
+      pip install mmcv-full==1.3.14 -f https://download.openmmlab.com/mmcv/dist/cu102/torch1.9.0/index.html -U -q    
+      ;; 
+
+   cuda11)  
+      echo "Installing icevision + dependencices for ${1}"
+      echo "- Installing torch and its dependencies"
+      pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 -f https://download.pytorch.org/whl/torch_stable.html --upgrade 
+
+      echo "- Installing mmcv"
+      pip install mmcv-full==1.3.14 -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.9.0/index.html -U -q  
+    ;;  
+    
+   cpu)  
+      echo "Installing icevision + dependencices for ${1}"
+      echo "- Installing torch and its dependencies"
+      pip install torch=="1.9.0+cpu" torchvision=="0.10.0+cpu" -f https://download.pytorch.org/whl/torch_stable.html  
+
+      echo "- Installing mmcv"
+      pip install mmcv-full=="1.3.14" -f https://download.openmmlab.com/mmcv/dist/cpu/torch1.9.0/index.html -U -q  
+    ;;
+
+   *)  
+      echo "Coud not install icevision. Check out which torch and torchvision versions are compatible with your CUDA version" 
+      exit -1 # Command to come out of the program with status -1
+      ;; 
+esac
+
+
+echo "- Installing mmdet"
+pip install mmdet==2.17.0 -U -q
+
+echo "- Installing icevision from master"
+pip install git+git://github.com/airctic/icevision.git\#egg=icevision[all] --upgrade -q
+
+echo "- Installing icedata from master"      
+pip install git+git://github.com/airctic/icedata.git --upgrade -q
+
+echo "- Installing yolov5-icevision" 
+pip install yolov5-icevision --upgrade -q 
+
+echo "icevision installation finished!"  
