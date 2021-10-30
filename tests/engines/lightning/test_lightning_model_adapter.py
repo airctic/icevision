ee92c16628ddcaf1787a94bc416209b9e3c331ea
frederik <fstroth>
2021-10-30 12:19:40 +0200

    (refactor) Now also ran black for test.

--- tests/engines/lightning/test_lightning_model_adapter.py
+++ tests/engines/lightning/test_lightning_model_adapter.py
@@ -1,5 +1,6 @@
 from icevision.engines.lightning import LightningModelAdapter
 
+
 class MockMetric:
     def __init__(self):
         self.name = "MockMetric"
@@ -17,11 +18,13 @@ class DummLightningModelAdapter(LightningModelAdapter):
 
 # test if finalize metrics reports metrics correctly
 def test_finalze_metrics_reports_metrics_correctly(mocker):
-    mocker.patch("icevision.engines.lightning.lightning_model_adapter.LightningModelAdapter.log")
+    mocker.patch(
+        "icevision.engines.lightning.lightning_model_adapter.LightningModelAdapter.log"
+    )
 
     adapter = DummLightningModelAdapter([MockMetric()], [("metric_a", "a")])
     adapter.finalize_metrics()
 
-    adapter.log.assert_any_call('a', 1, prog_bar=True)
-    adapter.log.assert_any_call('MockMetric/metric_a', 1)
-    adapter.log.assert_any_call('MockMetric/metric_b', 2)
+    adapter.log.assert_any_call("a", 1, prog_bar=True)
+    adapter.log.assert_any_call("MockMetric/metric_a", 1)
+    adapter.log.assert_any_call("MockMetric/metric_b", 2)
