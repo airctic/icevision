76a5641b58610a29d8628a86fce29d4b6e11041d
ganesh3 <ganeshbhat.3@gmail.com>
2021-12-19 17:55:31 +0530

    two_stage config options enabled

--- icevision/models/mmdet/common/bbox/two_stage/model.py
+++ icevision/models/mmdet/common/bbox/two_stage/model.py
@@ -13,6 +13,7 @@ def model(
     num_classes: int,
     checkpoints_path: Optional[Union[str, Path]] = "checkpoints",
     force_download=False,
+    cfg_options=None,
 ) -> nn.Module:
 
     cfg, weights_path = create_model_config(
@@ -20,6 +21,7 @@ def model(
         pretrained=backbone.pretrained,
         checkpoints_path=checkpoints_path,
         force_download=force_download,
+        cfg_options=None,
     )
 
     return build_model(
@@ -29,4 +31,5 @@ def model(
         pretrained=backbone.pretrained,
         checkpoints_path=checkpoints_path,
         force_download=force_download,
+        cfg_options=cfg_options,
     )
