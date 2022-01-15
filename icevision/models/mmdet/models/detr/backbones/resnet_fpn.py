05654309962076accff3f9934b8cb996f2bdfd23
Hector Lopez <hlopez@iti.es>
2022-01-15 10:41:14 +0100

    refactor: Changed config neme to MMDetDetrBackboneConfig

--- icevision/models/mmdet/models/detr/backbones/resnet_fpn.py
+++ icevision/models/mmdet/models/detr/backbones/resnet_fpn.py
@@ -6,7 +6,7 @@ from icevision.imports import *
 from icevision.models.mmdet.utils import *
 
 
-class MMDetRetinanetBackboneConfig(MMDetBackboneConfig):
+class MMDetDetrBackboneConfig(MMDetBackboneConfig):
     def __init__(self, **kwargs):
         super().__init__(model_name="detr", **kwargs)
 
@@ -14,7 +14,7 @@ class MMDetRetinanetBackboneConfig(MMDetBackboneConfig):
 base_config_path = mmdet_configs_path / "detr"
 base_weights_url = "http://download.openmmlab.com/mmdetection/v2.0/detr"
 
-r50_8x2_150e_coco = MMDetRetinanetBackboneConfig(
+r50_8x2_150e_coco = MMDetDetrBackboneConfig(
     config_path=base_config_path / "detr_r50_8x2_150e_coco.py",
     weights_url=f"{base_weights_url}/detr_r50_8x2_150e_coco/detr_r50_8x2_150e_coco_20201130_194835-2c4b8974.pth",
 )
