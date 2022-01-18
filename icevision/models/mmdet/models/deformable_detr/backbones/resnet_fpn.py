6d3db4fe576d0f16e7a5b039f4e17ac2c09fca39
dnth <dickson.neoh@gmail.com>
2022-01-18 18:08:11 +0800

    add refine deformable detr

--- icevision/models/mmdet/models/deformable_detr/backbones/resnet_fpn.py
+++ icevision/models/mmdet/models/deformable_detr/backbones/resnet_fpn.py
@@ -1,5 +1,6 @@
 __all__ = [
     "r50_16x2_50e_coco",
+    "refine_r50_16x2_50e_coco",
 ]
 
 from icevision.imports import *
@@ -19,6 +20,7 @@ r50_16x2_50e_coco = MMDetDeformableDetrBackboneConfig(
     weights_url=f"{base_weights_url}/deformable_detr_r50_16x2_50e_coco/deformable_detr_r50_16x2_50e_coco_20210419_220030-a12b9512.pth",
 )
 
-
-# https://github.com/open-mmlab/mmdetection/tree/master/configs/deformable_detr/deformable_detr_r50_16x2_50e_coco.py
-# https://download.openmmlab.com/mmdetection/v2.0/deformable_detr/deformable_detr_r50_16x2_50e_coco/deformable_detr_r50_16x2_50e_coco_20210419_220030-a12b9512.pth
+refine_r50_16x2_50e_coco = MMDetDeformableDetrBackboneConfig(
+    config_path=base_config_path / "deformable_detr_refine_r50_16x2_50e_coco.py",
+    weights_url=f"{base_weights_url}/deformable_detr_refine_r50_16x2_50e_coco/deformable_detr_refine_r50_16x2_50e_coco_20210419_220503-5f5dff21.pth",
+)
