cb3cb7be1582f3713b780fe8c62723934042f530
dnth <dickson.neoh@gmail.com>
2022-01-20 13:39:53 +0800

    rename swin backbone variables

--- icevision/models/mmdet/models/vfnet/backbones/swin.py
+++ icevision/models/mmdet/models/vfnet/backbones/swin.py
@@ -1,8 +1,8 @@
 __all__ = [
-    "mask_rcnn_swin_t_p4_w7_fpn_1x_coco",
-    "mask_rcnn_swin_t_p4_w7_fpn_ms_crop_3x_coco",
-    "mask_rcnn_swin_t_p4_w7_fpn_fp16_ms_crop_3x_coco",
-    "mask_rcnn_swin_s_p4_w7_fpn_fp16_ms_crop_3x_coco",
+    "swin_t_p4_w7_fpn_1x_coco",
+    "swin_t_p4_w7_fpn_ms_crop_3x_coco",
+    "swin_t_p4_w7_fpn_fp16_ms_crop_3x_coco",
+    "swin_s_p4_w7_fpn_fp16_ms_crop_3x_coco",
 ]
 
 from icevision.imports import *
@@ -20,22 +20,22 @@ base_weights_url = (
 )
 
 
-mask_rcnn_swin_t_p4_w7_fpn_1x_coco = MMDetSwinBackboneConfig(
+swin_t_p4_w7_fpn_1x_coco = MMDetSwinBackboneConfig(
     config_path=base_config_path / "mask_rcnn_swin-t-p4-w7_fpn_1x_coco.py",
     weights_url=f"{base_weights_url}/mask_rcnn_swin-t-p4-w7_fpn_1x_coco/mask_rcnn_swin-t-p4-w7_fpn_1x_coco_20210902_120937-9d6b7cfa.pth",
 )
 
-mask_rcnn_swin_t_p4_w7_fpn_ms_crop_3x_coco = MMDetSwinBackboneConfig(
+swin_t_p4_w7_fpn_ms_crop_3x_coco = MMDetSwinBackboneConfig(
     config_path=base_config_path / "mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_coco.py",
     weights_url=f"{base_weights_url}/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_coco/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_coco_20210906_131725-bacf6f7b.pth",
 )
 
-mask_rcnn_swin_t_p4_w7_fpn_fp16_ms_crop_3x_coco = MMDetSwinBackboneConfig(
+swin_t_p4_w7_fpn_fp16_ms_crop_3x_coco = MMDetSwinBackboneConfig(
     config_path=base_config_path / "mask_rcnn_swin-t-p4-w7_fpn_fp16_ms-crop-3x_coco.py",
     weights_url=f"{base_weights_url}/mask_rcnn_swin-t-p4-w7_fpn_fp16_ms-crop-3x_coco/mask_rcnn_swin-t-p4-w7_fpn_fp16_ms-crop-3x_coco_20210908_165006-90a4008c.pth",
 )
 
-mask_rcnn_swin_s_p4_w7_fpn_fp16_ms_crop_3x_coco = MMDetSwinBackboneConfig(
+swin_s_p4_w7_fpn_fp16_ms_crop_3x_coco = MMDetSwinBackboneConfig(
     config_path=base_config_path / "mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_coco.py",
     weights_url=f"{base_weights_url}/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_coco/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_coco_20210903_104808-b92c91f1.pth",
 )
