7a8addfb9f9255d748cdfe0e45f659fb0ff4eaea
dnth <dickson.neoh@gmail.com>
2022-01-23 17:13:07 +0800

    add swin small config

--- icevision/models/mmdet/models/vfnet/backbones/swin.py
+++ icevision/models/mmdet/models/vfnet/backbones/swin.py
@@ -1,4 +1,9 @@
-__all__ = ["swin_t_p4_w7_fpn_1x_coco"]
+__all__ = [
+    "swin_t_p4_w7_fpn_1x_coco",
+    "swin_s_p4_w7_fpn_1x_coco",
+    "swin_b_p4_w7_fpn_1x_coco",
+    "swin_l_p4_w7_fpn_1x_coco",
+]
 
 from icevision.imports import *
 from icevision.models.mmdet.utils import *
@@ -25,8 +30,24 @@ with open(base_config_path / "vfnet_swin_tiny_patch4_window7_224.py", "wb") as d
     download.write(content)
 #####
 
+#####
+# Download congi from URL.
+config_url = "https://raw.githubusercontent.com/dnth/mmdetection_configs/custom-configs/configs/vfnet/vfnet_swin_small_patch4_window7_224.py"
+with urlopen(config_url) as webpage:
+    content = webpage.read()
+# Save to local mmdet config folder.
+with open(base_config_path / "vfnet_swin_tiny_patch4_window7_224.py", "wb") as download:
+    download.write(content)
+#####
+
 swin_t_p4_w7_fpn_1x_coco = MMDetSwinBackboneConfig(
     config_path=base_config_path
     / "vfnet_swin_tiny_patch4_window7_224.py",  # Need to host this online on github
     weights_url="https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_tiny_patch4_window7_224.pth",
+)
+
+swin_s_p4_w7_fpn_1x_coco = MMDetSwinBackboneConfig(
+    config_path=base_config_path
+    / "vfnet_swin_small_patch4_window7_224.py",  # Need to host this online on github
+    weights_url="https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_small_patch4_window7_224.pth",
 )
