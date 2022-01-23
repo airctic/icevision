5181910f7b24153796001efe55327510d99f1778
dnth <dickson.neoh@gmail.com>
2022-01-23 18:08:06 +0800

    add base

--- icevision/models/mmdet/models/vfnet/backbones/swin.py
+++ icevision/models/mmdet/models/vfnet/backbones/swin.py
@@ -1,4 +1,8 @@
-__all__ = ["swin_t_p4_w7_fpn_1x_coco", "swin_s_p4_w7_fpn_1x_coco"]
+__all__ = [
+    "swin_t_p4_w7_fpn_1x_coco",
+    "swin_s_p4_w7_fpn_1x_coco",
+    "swin_b_p4_w7_fpn_1x_coco",
+]
 
 from icevision.imports import *
 from icevision.models.mmdet.utils import *
@@ -23,10 +27,8 @@ with urlopen(config_url) as webpage:
 # Save to local mmdet config folder.
 with open(base_config_path / "vfnet_swin_tiny_patch4_window7_224.py", "wb") as download:
     download.write(content)
-#####
 
-#####
-# Download congi from URL.
+
 config_url = "https://raw.githubusercontent.com/dnth/mmdetection_configs/custom-configs/configs/swin/vfnet_swin_small_patch4_window7_224.py"
 with urlopen(config_url) as webpage:
     content = webpage.read()
@@ -35,7 +37,16 @@ with open(
     base_config_path / "vfnet_swin_small_patch4_window7_224.py", "wb"
 ) as download:
     download.write(content)
-#####
+
+
+config_url = "https://raw.githubusercontent.com/dnth/mmdetection_configs/custom-configs/configs/swin/vfnet_base_small_patch4_window7_224.py"
+with urlopen(config_url) as webpage:
+    content = webpage.read()
+# Save to local mmdet config folder.
+with open(
+    base_config_path / "vfnet_base_small_patch4_window7_224.py", "wb"
+) as download:
+    download.write(content)
 
 swin_t_p4_w7_fpn_1x_coco = MMDetSwinBackboneConfig(
     config_path=base_config_path
@@ -47,4 +58,10 @@ swin_s_p4_w7_fpn_1x_coco = MMDetSwinBackboneConfig(
     config_path=base_config_path
     / "vfnet_swin_small_patch4_window7_224.py",  # Need to host this online on github
     weights_url="https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_small_patch4_window7_224.pth",
+)
+
+swin_b_p4_w7_fpn_1x_coco = MMDetSwinBackboneConfig(
+    config_path=base_config_path
+    / "vfnet_swin_base_patch4_window7_224.py",  # Need to host this online on github
+    weights_url="https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_base_patch4_window7_224.pth",
 )
