118966fd9a657a14e5df9066dbbaf06661764222
dnth <dickson.neoh@gmail.com>
2022-01-25 15:57:08 +0800

    add resnet backbone for yolact

--- icevision/models/mmdet/models/yolact/backbones/resnet_fpn.py
+++ icevision/models/mmdet/models/yolact/backbones/resnet_fpn.py
@@ -1,4 +1,4 @@
-__all__ = ["yolact_r50_1x8_coco"]
+__all__ = ["r50_1x8_coco", "r50_8x8_coco", "r101_1x8_coco"]
 
 from icevision.imports import *
 from icevision.models.mmdet.utils import *
@@ -12,7 +12,17 @@ class YOLOACTMMDetBackboneConfig(MMDetBackboneConfig):
 base_config_path = mmdet_configs_path / "yolact"
 base_weights_url = "http://download.openmmlab.com/mmdetection/v2.0/yolact"
 
-yolact_r50_1x8_coco = YOLOACTMMDetBackboneConfig(
+r50_1x8_coco = YOLOACTMMDetBackboneConfig(
     config_path=base_config_path / "yolact_r50_1x8_coco.py",
     weights_url=f"{base_weights_url}/yolact_r50_1x8_coco/yolact_r50_1x8_coco_20200908-f38d58df.pth",
+)
+
+r50_8x8_coco = YOLOACTMMDetBackboneConfig(
+    config_path=base_config_path / "yolact_r50_8x8_coco.py",
+    weights_url=f"{base_weights_url}/yolact_r50_8x8_coco/yolact_r50_8x8_coco_20200908-ca34f5db.pth",
+)
+
+r101_1x8_coco = YOLOACTMMDetBackboneConfig(
+    config_path=base_config_path / "yolact_r101_1x8_coco.py",
+    weights_url=f"{base_weights_url}/yolact_r101_1x8_coco/yolact_r101_1x8_coco_20200908-4cbe9101.pth",
 )
