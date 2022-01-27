f3e641aa186c0de1f86b634ba1ba376d9bf34b9d
dnth <dickson.neoh@gmail.com>
2022-01-27 15:41:46 +0800

    add centripetalnet in nb

--- icevision/models/mmdet/models/centripetalnet/backbones/hourglass.py
+++ icevision/models/mmdet/models/centripetalnet/backbones/hourglass.py
@@ -1,4 +1,4 @@
-__all__ = ["centripetalnet_hourglass104_mstest_16x6_210e_coco"]
+__all__ = ["hourglass104_mstest_16x6_210e_coco"]
 
 from icevision.imports import *
 from icevision.models.mmdet.utils import *
@@ -12,7 +12,7 @@ class MMDetCentripetalnetBackboneConfig(MMDetBackboneConfig):
 base_config_path = mmdet_configs_path / "centripetalnet"
 base_weights_url = "http://download.openmmlab.com/mmdetection/v2.0/centripetalnet"
 
-centripetalnet_hourglass104_mstest_16x6_210e_coco = MMDetCentripetalnetBackboneConfig(
+hourglass104_mstest_16x6_210e_coco = MMDetCentripetalnetBackboneConfig(
     config_path=base_config_path
     / "centripetalnet_hourglass104_mstest_16x6_210e_coco.py",
     weights_url=f"{base_weights_url}/centripetalnet_hourglass104_mstest_16x6_210e_coco/centripetalnet_hourglass104_mstest_16x6_210e_coco_20200915_204804-3ccc61e5.pth",
