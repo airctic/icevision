4460a719f7689a342dfddf2540d34462661df5ec
dnth <dickson.neoh@gmail.com>
2022-06-15 14:50:43 +0800

    verified all retinanet backbone working

--- icevision/models/torchvision/retinanet/backbones/resnet_fpn_configs.py
+++ icevision/models/torchvision/retinanet/backbones/resnet_fpn_configs.py
@@ -1,13 +1,13 @@
 __all__ = [
-    # "resnet18_fpn",
-    # "resnet34_fpn",
+    "resnet18_fpn",
+    "resnet34_fpn",
     "resnet50_fpn",
-    # "resnet101_fpn",
-    # "resnet152_fpn",
-    # "resnext50_32x4d_fpn",
-    # "resnext101_32x8d_fpn",
-    # "wide_resnet50_2_fpn",
-    # "wide_resnet101_2_fpn",
+    "resnet101_fpn",
+    "resnet152_fpn",
+    "resnext50_32x4d_fpn",
+    "resnext101_32x8d_fpn",
+    "wide_resnet50_2_fpn",
+    "wide_resnet101_2_fpn",
 ]
 
 from icevision.models.torchvision.retinanet.backbones.resnet_fpn_utils import (
@@ -65,12 +65,20 @@ def wide_resnet101_2_fpn_fn(pretrained: bool = False, **kwargs):
     return _resnet_fpn("wide_resnet101_2", pretrained=pretrained, **kwargs)
 
 
-# resnet18_fpn = TorchvisionRetinanetBackboneConfig(backbone_fn=resnet18_fpn_fn)
-# resnet34_fpn = TorchvisionRetinanetBackboneConfig(resnet34_fpn_fn)
+resnet18_fpn = TorchvisionRetinanetBackboneConfig(backbone_fn=resnet18_fpn_fn)
+resnet34_fpn = TorchvisionRetinanetBackboneConfig(backbone_fn=resnet34_fpn_fn)
 resnet50_fpn = TorchvisionRetinanetBackboneConfig(backbone_fn=resnet50_fpn_fn)
-# resnet101_fpn = TorchvisionRetinanetBackboneConfig(resnet101_fpn_fn)
-# resnet152_fpn = TorchvisionRetinanetBackboneConfig(resnet152_fpn_fn)
-# resnext50_32x4d_fpn = TorchvisionRetinanetBackboneConfig(resnext50_32x4d_fpn_fn)
-# resnext101_32x8d_fpn = TorchvisionRetinanetBackboneConfig(resnext101_32x8d_fpn_fn)
-# wide_resnet50_2_fpn = TorchvisionRetinanetBackboneConfig(wide_resnet50_2_fpn_fn)
-# wide_resnet101_2_fpn = TorchvisionRetinanetBackboneConfig(wide_resnet101_2_fpn_fn)
+resnet101_fpn = TorchvisionRetinanetBackboneConfig(backbone_fn=resnet101_fpn_fn)
+resnet152_fpn = TorchvisionRetinanetBackboneConfig(backbone_fn=resnet152_fpn_fn)
+resnext50_32x4d_fpn = TorchvisionRetinanetBackboneConfig(
+    backbone_fn=resnext50_32x4d_fpn_fn
+)
+resnext101_32x8d_fpn = TorchvisionRetinanetBackboneConfig(
+    backbone_fn=resnext101_32x8d_fpn_fn
+)
+wide_resnet50_2_fpn = TorchvisionRetinanetBackboneConfig(
+    backbone_fn=wide_resnet50_2_fpn_fn
+)
+wide_resnet101_2_fpn = TorchvisionRetinanetBackboneConfig(
+    backbone_fn=wide_resnet101_2_fpn_fn
+)
