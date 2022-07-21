94dcc4740a0297883f593305d7739d5f68af0b95
frederik <fstroth>
2022-07-21 19:34:56 +0200

    (Refactor) Changed the names of model_type_name and backend_lib_name to model_name and lib_name.

--- icevision/utils/model_creation.py
+++ icevision/utils/model_creation.py
@@ -66,13 +66,13 @@ def get_backend_libs(base_path: str = BASE_PATH) -> List[str]:
 
 
 def get_model_types_for_backend_lib(
-    backend_lib_name: str, base_path: str = BASE_PATH
+    lib_name: str, base_path: str = BASE_PATH
 ) -> List[str]:
     """Returns all available model_types for a given backend lib
 
     Parameters
     ----------
-    backend_lib_name: str
+    lib_name: str
         Name of the backend_lib to get the model types of
     base_path: str
         Path from where to read out the available models from
@@ -84,23 +84,23 @@ def get_model_types_for_backend_lib(
     """
     model_types = [
         i
-        for i in os.listdir(os.path.join(base_path, backend_lib_name))
-        if os.path.isdir(os.path.join(base_path, backend_lib_name, i))
+        for i in os.listdir(os.path.join(base_path, lib_name))
+        if os.path.isdir(os.path.join(base_path, lib_name, i))
         and not i.startswith("__")
     ]
     return model_types
 
 
 def get_backbone_names(
-    backend_lib_name: str, model_type_name: str, base_path: str = BASE_PATH
+    lib_name: str, model_name: str, base_path: str = BASE_PATH
 ) -> List[str]:
     """Returns all available backbones for a given backend lib
 
     Parameters
     ----------
-    backend_lib_name: str
+    lib_name: str
         Name of the backend_lib to get the backbones for
-    model_type_name: str
+    model_name: str
         Name of the model type to get the backbones for
     base_path: str
         Path from where to read out the available models from
@@ -110,23 +110,17 @@ def get_backbone_names(
     backbones: List[str]
         List of available backbones
     """
-    print(backend_lib_name)
-    print(model_type_name)
     backbone_files = [
         i
-        for i in os.listdir(
-            os.path.join(base_path, backend_lib_name, model_type_name, "backbones")
-        )
-        if os.path.join(
-            base_path, backend_lib_name, model_type_name, "backbones", i
-        ).endswith(".py")
+        for i in os.listdir(os.path.join(base_path, lib_name, model_name, "backbones"))
+        if os.path.join(base_path, lib_name, model_name, "backbones", i).endswith(".py")
         and not i.startswith("__")
     ]
     backbone_list = []
     for backbone_file in backbone_files:
         backbones = importlib.import_module(
             "icevision.models."
-            + ".".join([backend_lib_name, model_type_name])
+            + ".".join([lib_name, model_name])
             + ".backbones."
             + backbone_file.replace(".py", "")
         )
@@ -135,8 +129,8 @@ def get_backbone_names(
 
 
 def load_model_components(
-    backend_lib_name: str,
-    model_type_name: str,
+    lib_name: str,
+    model_name: str,
     backbone_name: str,
     model_base_path: str = BASE_PATH,
 ):
@@ -144,9 +138,9 @@ def load_model_components(
 
     Parameters
     ----------
-    backend_lib_name: str
+    lib_name: str
         Name of the backend_lib to use
-    model_type_name: str
+    model_name: str
         Name of the model type to use
     backbone_name: str
         Name of the backbone to use
@@ -160,37 +154,35 @@ def load_model_components(
     """
     # load the backend lib
     try:
-        backend_lib = get_module_element_form_module(models, backend_lib_name)
+        backend_lib = get_module_element_form_module(models, lib_name)
     except AttributeError:
         raise AttributeError(
-            f"Backend lib: {backend_lib_name} not found. Possible options are: {get_backend_libs(model_base_path)}"
+            f"Backend lib: {lib_name} not found. Possible options are: {get_backend_libs(model_base_path)}"
         )
     # load the model_type
     try:
-        model_type = get_module_element_form_module(
-            models, backend_lib_name, model_type_name
-        )
+        model_type = get_module_element_form_module(models, lib_name, model_name)
     except AttributeError:
         raise AttributeError(
-            f"Model type: {model_type_name} not found. Possible options are: {get_model_types_for_backend_lib(model_base_path, backend_lib_name)}"
+            f"Model type: {model_name} not found. Possible options are: {get_model_types_for_backend_lib(model_base_path, lib_name)}"
         )
     # load backbone
     try:
         backbone_module = get_module_element_form_module(
-            models, backend_lib_name, model_type_name, "backbones"
+            models, lib_name, model_name, "backbones"
         )
         backbone = getattr(backbone_module, backbone_name)
     except AttributeError:
         raise AttributeError(
-            f"Backbone: {backbone_name} not found. Possible options are: {get_backbone_names(backend_lib_name, model_type_name)}"
+            f"Backbone: {backbone_name} not found. Possible options are: {get_backbone_names(lib_name, model_name)}"
         )
 
     return model_type, backbone
 
 
 def build_model(
-    backend_lib_name: str,
-    model_type_name: str,
+    lib_name: str,
+    model_name: str,
     backbone_name: str,
     num_classes: int,
     backbone_config: Optional[dict] = None,
@@ -200,9 +192,9 @@ def build_model(
 
     Parameters
     ----------
-    backend_lib_name: str
+    lib_name: str
         Name of the backend_lib to use
-    model_type_name: str
+    model_name: str
         Name of the model type to use
     backbone_name: str
         Name of the backbone to use
@@ -223,9 +215,7 @@ def build_model(
         backbone_config = {"pretrained": True}
     if model_config is None:
         model_config = {}
-    model_type, backbone = load_model_components(
-        backend_lib_name, model_type_name, backbone_name
-    )
+    model_type, backbone = load_model_components(lib_name, model_name, backbone_name)
     model = model_type.model(
         backbone=backbone(**backbone_config), num_classes=num_classes, **model_config
     )
